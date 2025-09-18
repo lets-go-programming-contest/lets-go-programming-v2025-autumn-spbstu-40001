683e41f8826c23f503c430d757d529f6d21020de
Aapng-cmd <verart1@yandex.ru>
2025-09-18 10:49:59 -0500

    Task itself

--- artemiy.vernikovskiy/task-1/cmd/service/main.go
+++ artemiy.vernikovskiy/task-1/cmd/service/main.go
@@ -5,6 +5,42 @@ import (
 )
 
 func main() {
+    var a int = 0
+    var b int = 0
     
+    _, err := fmt.Scan(&a)
+    if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+    _, err := fmt.Scan(&b)
+    if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	
+	var oper string
+	_, err := fmt.Scan(&oper)
+    if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+	
+	switch oper {
+	case "+":
+	    fmt.Println(a + b)
+	case "-":
+	    fmt.Println(a - b)
+	case "*":
+	    fmt.Println(a + b)
+	case "/":
+	    switch b {
+	    case 0:
+	        fmt.Println("Division by zero")
+	    default:
+	        fmt.Println(a / b)
+	    }
+	default:
+	    fmt.Println("Invalid operation")
+	}
 }
-
