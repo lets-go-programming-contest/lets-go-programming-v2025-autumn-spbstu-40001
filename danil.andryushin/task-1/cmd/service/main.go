bf54bd735bcf175c67a7e97918e29be41b6f74f9
atroxxx <danilandriuschin@yandex.ru>
2025-09-18 18:59:21 +0300

    [TASK-1] main logic implementation

--- danil.andryushin/task-1/cmd/service/main.go
+++ danil.andryushin/task-1/cmd/service/main.go
@@ -2,6 +2,42 @@ package main
 
 import "fmt"
 
+func calc(a, b int, operation string) (int, error) {
+	switch operation {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		if b == 0 {
+			return 0, fmt.Errorf("Devision by zero")
+		}
+		return a / b, nil
+	default:
+		return 0, fmt.Errorf("Invalid operation")
+	}
+}
+
 func main() {
-	fmt.Println("task-1")
+	var (
+		lhs, rhs  int    = 0, 0
+		operation string = ""
+	)
+	scanned, _ := fmt.Scan(&lhs, &rhs, &operation)
+	switch scanned {
+	case 0:
+		fmt.Println("Invalid first operand")
+	case 1:
+		fmt.Println("Invalid second operand")
+	case 2:
+		fmt.Println("Operation input error")
+	}
+	result, err := calc(lhs, rhs, operation)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(result)
 }
