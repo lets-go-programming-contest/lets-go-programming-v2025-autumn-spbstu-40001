4ca7d685748bc3733d790edcc3cdae82ec238be9
Richmick <ry4ckov.misha2018@yandex.ru>
2025-09-18 23:47:29 +0300

    [TASK-1] Add input

--- mihail.rychkov/task-1/cmd/service/main.go
+++ mihail.rychkov/task-1/cmd/service/main.go
@@ -1,3 +1,23 @@
-package main
+package main;
 
-func main() {}
+import "fmt";
+
+func main() {
+	var (
+		lhs, rhs int = 0, 0;
+		operation string = "";
+	);
+
+	scanned, _ := fmt.Scan(&lhs, &rhs, &operation);
+	switch scanned {
+	case 0:
+		fmt.Println("Invalid first operand");
+		return;
+	case 1:
+		fmt.Println("Invalid second operand");
+		return;
+	case 2:
+		fmt.Println("Invalid operation");
+		return;
+	}
+}
