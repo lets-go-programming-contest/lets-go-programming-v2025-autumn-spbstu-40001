b858d56d86704a229528a9f33a22eef66d8f5297
Aapng-cmd <verart1@yandex.ru>
2025-09-18 11:00:36 -0500

    [TASK-1] Fixed commiting script and tried to understand what gofmt wants

--- artemiy.vernikovskiy/task-1/cmd/service/main.go
+++ artemiy.vernikovskiy/task-1/cmd/service/main.go
@@ -7,25 +7,26 @@ import (
 func main() {
     var a int = 0
     var b int = 0
-    
+
     _, err := fmt.Scan(&a)
     if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
+
     _, err = fmt.Scan(&b)
     if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
-	
+
 	var oper string
 	_, err = fmt.Scan(&oper)
     if err != nil {
 		fmt.Println("Invalid operation")
 		return
 	}
-	
+
 	switch oper {
 	case "+":
 	    fmt.Println(a + b)
