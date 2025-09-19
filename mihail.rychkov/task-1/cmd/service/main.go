c50ac060ad6124bcb1000b0d72cb6e4678bee791
Richmick <ry4ckov.misha2018@yandex.ru>
2025-09-19 08:15:54 +0300

    [TASK-1] Move errors to global scope

--- mihail.rychkov/task-1/cmd/service/main.go
+++ mihail.rychkov/task-1/cmd/service/main.go
@@ -1,47 +1,53 @@
-package main;
+package main
 
-import "fmt";
+import "fmt"
+import "errors"
+
+var (
+	ZeroDivisionError = errors.New("Division by zero")
+	InvalidOperationError = errors.New("Invalid operation")
+)
 
 func evaluate(lhs int, operation string, rhs int) (int, error) {
 	switch operation {
 	case "+":
-		return lhs + rhs, nil;
+		return lhs + rhs, nil
 	case "-":
-		return lhs - rhs, nil;
+		return lhs - rhs, nil
 	case "*":
-		return lhs * rhs, nil;
+		return lhs * rhs, nil
 	case "/":
 		if rhs == 0 {
-			return 0, fmt.Errorf("Division by zero");
+			return 0, ZeroDivisionError
 		}
-		return lhs / rhs, nil;
+		return lhs / rhs, nil
 	}
-	return 0, fmt.Errorf("Invalid operation");
+	return 0, InvalidOperationError
 }
 
 func main() {
 	var (
-		lhs, rhs int;
-		operation string;
-	);
+		lhs, rhs int
+		operation string
+	)
 
-	scanned, _ := fmt.Scan(&lhs, &rhs, &operation);
+	scanned, _ := fmt.Scan(&lhs, &rhs, &operation)
 	switch scanned {
 	case 0:
-		fmt.Println("Invalid first operand");
-		return;
+		fmt.Println("Invalid first operand")
+		return
 	case 1:
-		fmt.Println("Invalid second operand");
-		return;
+		fmt.Println("Invalid second operand")
+		return
 	case 2:
-		fmt.Println("Invalid operation");
-		return;
+		fmt.Println("Invalid operation")
+		return
 	}
 
-	res, err := evaluate(lhs, operation, rhs);
+	res, err := evaluate(lhs, operation, rhs)
 	if err != nil {
-		fmt.Println(err);
-		return;
+		fmt.Println(err)
+		return
 	}
-	fmt.Println(res);
+	fmt.Println(res)
 }
