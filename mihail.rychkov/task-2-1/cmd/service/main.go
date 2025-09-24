981700577c261da5599db49e9b28b76279254a41
Richmick <ry4ckov.misha2018@yandex.ru>
2025-09-24 21:25:27 +0300

    [TASK-2-1] Implement main logic

--- mihail.rychkov/task-2-1/cmd/service/main.go
+++ mihail.rychkov/task-2-1/cmd/service/main.go
@@ -1,4 +1,56 @@
 package main;
 
+import "fmt";
+
 func main() {
+	var nDepartments int;
+
+	_, err := fmt.Scan(&nDepartments);
+	if (err != nil) {
+		fmt.Println("Failed to read departments count");
+		fmt.Println(err);
+		return;
+	}
+
+	for _ = range nDepartments {
+		var nEmployees int;
+
+		_, err = fmt.Scan(&nEmployees);
+		if (err != nil) {
+			fmt.Println("Failed to read employees count");
+			fmt.Println(err);
+			return;
+		}
+
+		var minTemperature, maxTemperature int = 15, 30;
+		for _ = range nEmployees {
+			var (
+				sign string;
+				temperature int;
+			);
+
+			_, err = fmt.Scan(&sign, &temperature);
+			if (err != nil) {
+				fmt.Println("Failed to read employee's wish");
+				fmt.Println(err);
+				return;
+			}
+
+			switch sign {
+			case ">=":
+				minTemperature = max(minTemperature, temperature);
+			case "<=":
+				maxTemperature = min(maxTemperature, temperature);
+			default:
+				fmt.Println("Unknown comparison sign");
+				return;
+			}
+
+			if (minTemperature <= maxTemperature) {
+				fmt.Println(minTemperature);
+			} else {
+				fmt.Println(-1);
+			}
+		}
+	}
 }
