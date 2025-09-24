998a526fa8d6efa4e33468a050d4ff0d43661c34
Richmick <ry4ckov.misha2018@yandex.ru>
2025-09-24 21:31:35 +0300

    [TASK-2-1] Apply gofmt

--- mihail.rychkov/task-2-1/cmd/service/main.go
+++ mihail.rychkov/task-2-1/cmd/service/main.go
@@ -1,55 +1,55 @@
-package main;
+package main
 
-import "fmt";
+import "fmt"
 
 func main() {
-	var nDepartments int;
+	var nDepartments int
 
-	_, err := fmt.Scan(&nDepartments);
-	if (err != nil) {
-		fmt.Println("Failed to read departments count");
-		fmt.Println(err);
-		return;
+	_, err := fmt.Scan(&nDepartments)
+	if err != nil {
+		fmt.Println("Failed to read departments count")
+		fmt.Println(err)
+		return
 	}
 
 	for _ = range nDepartments {
-		var nEmployees int;
+		var nEmployees int
 
-		_, err = fmt.Scan(&nEmployees);
-		if (err != nil) {
-			fmt.Println("Failed to read employees count");
-			fmt.Println(err);
-			return;
+		_, err = fmt.Scan(&nEmployees)
+		if err != nil {
+			fmt.Println("Failed to read employees count")
+			fmt.Println(err)
+			return
 		}
 
-		var minTemperature, maxTemperature int = 15, 30;
+		var minTemperature, maxTemperature int = 15, 30
 		for _ = range nEmployees {
 			var (
-				sign string;
-				temperature int;
-			);
+				sign        string
+				temperature int
+			)
 
-			_, err = fmt.Scan(&sign, &temperature);
-			if (err != nil) {
-				fmt.Println("Failed to read employee's wish");
-				fmt.Println(err);
-				return;
+			_, err = fmt.Scan(&sign, &temperature)
+			if err != nil {
+				fmt.Println("Failed to read employee's wish")
+				fmt.Println(err)
+				return
 			}
 
 			switch sign {
 			case ">=":
-				minTemperature = max(minTemperature, temperature);
+				minTemperature = max(minTemperature, temperature)
 			case "<=":
-				maxTemperature = min(maxTemperature, temperature);
+				maxTemperature = min(maxTemperature, temperature)
 			default:
-				fmt.Println("Unknown comparison sign");
-				return;
+				fmt.Println("Unknown comparison sign")
+				return
 			}
 
-			if (minTemperature <= maxTemperature) {
-				fmt.Println(minTemperature);
+			if minTemperature <= maxTemperature {
+				fmt.Println(minTemperature)
 			} else {
-				fmt.Println(-1);
+				fmt.Println(-1)
 			}
 		}
 	}
