d8a2f90533ed7a1ea88aaf3c221a693b1c727189
Richmick <ry4ckov.misha2018@yandex.ru>
2025-09-25 13:21:56 +0300

    [TASK-2-2] Apply linter fix

--- mihail.rychkov/task-2-2/cmd/service/main.go
+++ mihail.rychkov/task-2-2/cmd/service/main.go
@@ -1,68 +1,90 @@
-package main;
+package main
 
-import "container/heap";
-import "fmt";
+import (
+	"container/heap"
+	"fmt"
+)
 
-type IntHeap []int;
+type IntHeap []int
 
-func (slice IntHeap) Len() int {
-	return len(slice);
+func (slice *IntHeap) Len() int {
+	return len(*slice)
 }
-func (slice IntHeap) Less(i, j int) bool {
-	return slice[i] > slice[j];
+
+func (slice *IntHeap) Less(i, j int) bool {
+	return (*slice)[i] > (*slice)[j]
 }
-func (slice IntHeap) Swap(i, j int) {
-	slice[i], slice[j] = slice[j], slice[i];
+
+func (slice *IntHeap) Swap(i, j int) {
+	(*slice)[i], (*slice)[j] = (*slice)[j], (*slice)[i]
 }
+
 func (slice *IntHeap) Push(value any) {
-	*slice = append(*slice, value.(int));
+	intValue, ok := value.(int)
+	if !ok {
+		return
+	}
+
+	*slice = append(*slice, intValue)
 }
+
 func (slice *IntHeap) Pop() any {
-	result := (*slice)[len(*slice) - 1];
-	*slice = (*slice)[0 : len(*slice) - 1];
-	return result;
+	result := (*slice)[len(*slice)-1]
+	*slice = (*slice)[0 : len(*slice)-1]
+
+	return result
 }
-func (slice IntHeap) Top() int {
-	return slice[0];
+
+func (slice *IntHeap) Top() int {
+	return (*slice)[0]
 }
 
 func main() {
-	var nDishes int;
-	_, err := fmt.Scan(&nDishes);
+	var nDishes int
+
+	_, err := fmt.Scan(&nDishes)
 	if err != nil {
-		fmt.Println("Failed to read dishes count");
-		fmt.Println(err);
-		return;
+		fmt.Println("Failed to read dishes count")
+		fmt.Println(err)
+
+		return
 	}
 
-	dishesQueue := &IntHeap{};
-	for range(nDishes) {
-		var dishValue int;
+	dishesQueue := &IntHeap{}
 
-		_, err = fmt.Scan(&dishValue);
+	for range nDishes {
+		var dishValue int
+
+		_, err = fmt.Scan(&dishValue)
 		if err != nil {
-			fmt.Println("Failed to read dish value");
-			fmt.Println(err);
-			return;
+			fmt.Println("Failed to read dish value")
+			fmt.Println(err)
+
+			return
 		}
 
-		heap.Push(dishesQueue, dishValue);
+		heap.Push(dishesQueue, dishValue)
 	}
 
-	var dishId int;
-	_, err = fmt.Scan(&dishId);
+	var dishId int
+
+	_, err = fmt.Scan(&dishId)
 	if err != nil {
-		fmt.Println("Failed to read priority number");
-		fmt.Println(err);
-		return;
-	}
-	if (dishId > dishesQueue.Len()) || (dishId <= 0) {
-		fmt.Println("Entered nonexistent priority number");
-		return;
+		fmt.Println("Failed to read priority number")
+		fmt.Println(err)
+
+		return
 	}
 
-	for range(dishId - 1) {
-		heap.Pop(dishesQueue);
+	if (dishId > dishesQueue.Len()) || (dishId <= 0) {
+		fmt.Println("Entered nonexistent priority number")
+
+		return
 	}
-	fmt.Println(dishesQueue.Top());
+
+	for range dishId - 1 {
+		heap.Pop(dishesQueue)
+	}
+
+	fmt.Println(dishesQueue.Top())
 }
