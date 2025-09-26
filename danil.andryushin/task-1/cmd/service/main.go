f1dd259481de6704efca20d023ef749e03f50a8d
atroxxx <danilandriuschin@yandex.ru>
2025-09-26 09:37:25 +0300

    [TASK-1] updade scan

--- danil.andryushin/task-1/cmd/service/main.go
+++ danil.andryushin/task-1/cmd/service/main.go
@@ -33,18 +33,23 @@ func main() {
 		lhs, rhs  int
 		operation string
 	)
-	scanned, err := fmt.Scan(&lhs, &rhs, &operation)
+
+	_, err := fmt.Scan(&lhs)
 	if err != nil {
-		switch scanned {
-		case 0:
-			fmt.Println("Invalid first operand")
-		case 1:
-			fmt.Println("Invalid second operand")
-		case 2:
-			fmt.Println("Invalid operation")
-		}
+		fmt.Println("Invalid first operand")
 		return
 	}
+	_, err = fmt.Scan(&rhs)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	_, err = fmt.Scan(&operation)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
 	result, err := calc(lhs, rhs, operation)
 	if err != nil {
 		fmt.Println(err)
