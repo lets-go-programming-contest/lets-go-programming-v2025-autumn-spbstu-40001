bbd29092b20e0cf5cecf7b2783e060db19f4bad5
Richmick <ry4ckov.misha2018@yandex.ru>
2025-09-28 20:53:32 +0300

    [TASK-2-1] Apply linters

--- mihail.rychkov/task-2-1/internal/mainproc/wish.go
+++ mihail.rychkov/task-2-1/internal/mainproc/wish.go
@@ -1,24 +1,29 @@
-package mainproc;
+package mainproc
 
-import "errors";
+import "errors"
 
 type Wish struct {
-	minTemperature, maxTemperature int;
+	minTemperature, maxTemperature int
 }
+
 func (wish *Wish) IncludeMin(temperature int) {
-	if (wish.minTemperature < temperature) {
-		wish.minTemperature = temperature;
+	if wish.minTemperature < temperature {
+		wish.minTemperature = temperature
 	}
 }
+
 func (wish *Wish) IncludeMax(temperature int) {
-	if (wish.maxTemperature > temperature) {
-		wish.maxTemperature = temperature;
+	if wish.maxTemperature > temperature {
+		wish.maxTemperature = temperature
 	}
 }
-var ErrNoOptimum = errors.New("optimal temperature does not exist");
+
+var ErrNoOptimum = errors.New("optimal temperature does not exist")
+
 func (wish *Wish) GetOptimum() (int, error) {
-	if (wish.minTemperature > wish.maxTemperature) {
-		return 0, ErrNoOptimum;
+	if wish.minTemperature > wish.maxTemperature {
+		return 0, ErrNoOptimum
 	}
-	return wish.minTemperature, nil;
+
+	return wish.minTemperature, nil
 }
