a4d599074c6e3e17001221cce16d2d9687de44cb
Richmick <ry4ckov.misha2018@yandex.ru>
2025-09-28 19:08:38 +0300

    [TASK-2-2] Apply linter

--- mihail.rychkov/task-2-2/pkg/primheap/internal.go
+++ mihail.rychkov/task-2-2/pkg/primheap/internal.go
@@ -1,36 +1,44 @@
-package primheap;
+package primheap
 
-import "cmp";
+import "cmp"
 
 type unwrappedPrimHeap[T cmp.Ordered] struct {
-	data []T;
-	comparator func(T, T) bool;
+	data       []T
+	comparator func(T, T) bool
 }
 
 func (objHeap *unwrappedPrimHeap[T]) Less(i, j int) bool {
-	if (objHeap.comparator != nil) {
-		return objHeap.comparator(objHeap.data[i], objHeap.data[j]);
+	if objHeap.comparator != nil {
+		return objHeap.comparator(objHeap.data[i], objHeap.data[j])
 	}
-	return cmp.Less(objHeap.data[i], objHeap.data[j]);
+
+	return cmp.Less(objHeap.data[i], objHeap.data[j])
 }
+
 func (objHeap *unwrappedPrimHeap[T]) Len() int {
-	return len(objHeap.data);
+	return len(objHeap.data)
 }
+
 func (objHeap *unwrappedPrimHeap[T]) Swap(i, j int) {
-	objHeap.data[i], objHeap.data[j] = objHeap.data[j], objHeap.data[i];
+	objHeap.data[i], objHeap.data[j] = objHeap.data[j], objHeap.data[i]
 }
+
 func (objHeap *unwrappedPrimHeap[T]) Push(value any) {
-	intValue, ok := value.(T);
-	if (!ok) {
-		panic("failed to cast heap.push value to stored type");
+	intValue, ok := value.(T)
+	if !ok {
+		panic("failed to cast heap.push value to stored type")
 	}
-	objHeap.data = append(objHeap.data, intValue);
+
+	objHeap.data = append(objHeap.data, intValue)
 }
+
 func (objHeap *unwrappedPrimHeap[T]) Pop() any {
-	if (objHeap.Len() == 0) {
-		return nil;
+	if objHeap.Len() == 0 {
+		return nil
 	}
-	result := objHeap.data[objHeap.Len() - 1];
-	objHeap.data = objHeap.data[0 : objHeap.Len() - 1];
-	return result;
+
+	result := objHeap.data[objHeap.Len()-1]
+	objHeap.data = objHeap.data[0 : objHeap.Len()-1]
+
+	return result
 }
