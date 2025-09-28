58d279979db1fc8d5d3f98907220e1dd76911df2
Nekich06 <p.nikita.73526@gmail.com>
2025-09-28 15:14:03 +0300

    [TASK-2-1] Made it simplier

--- nikita.petrov/task-2-1/cmd/service/main.go
+++ nikita.petrov/task-2-1/cmd/service/main.go
@@ -6,7 +6,6 @@ type TempManager struct {
 	maxTemp int
 	minTemp int
 	optTemp int
-	keepMin bool
 }
 
 func main() {
@@ -20,59 +19,24 @@ func main() {
 			condition             string
 		)
 
-		airConditioner := TempManager{30, 15, 0, false}
+		airConditioner := TempManager{30, 15, 0}
 
 		fmt.Scan(&staffNum)
-		fmt.Scan(&condition)
-		fmt.Scan(&wishfulTemp)
 
-		if !turnOnTempManager(&airConditioner, condition, wishfulTemp) {
-			return
-		}
-
-		fmt.Println(airConditioner.optTemp)
-
-		for j := 1; j < staffNum; j++ {
+		for j := 0; j < staffNum; j++ {
 			fmt.Scan(&condition)
 			fmt.Scan(&wishfulTemp)
 
-			if !changeStatus(&airConditioner, condition, wishfulTemp) {
-				return
+			if changeStatus(&airConditioner, condition, wishfulTemp) {
+				fmt.Println(airConditioner.optTemp)
+			} else {
+				fmt.Println(-1)
 			}
-
-			fmt.Println(airConditioner.optTemp)
 		}
-	}
-}
-
-func turnOnTempManager(someTM *TempManager, condition string, initialTemp int) bool {
-	if recalculateMinMax(someTM, condition, initialTemp) {
-		if condition == ">=" {
-			someTM.keepMin = true
-		} else {
-			someTM.keepMin = false
-		}
-		someTM.optTemp = initialTemp
-		return true
-	} else {
-		return false
 	}
 }
 
 func changeStatus(someTM *TempManager, condition string, newTemp int) bool {
-	if recalculateMinMax(someTM, condition, newTemp) {
-		if someTM.keepMin {
-			someTM.optTemp = someTM.minTemp
-		} else {
-			someTM.optTemp = someTM.maxTemp
-		}
-		return true
-	} else {
-		return false
-	}
-}
-
-func recalculateMinMax(someTM *TempManager, condition string, newTemp int) bool {
 	switch condition {
 	case "<=":
 		if newTemp >= someTM.minTemp {
@@ -80,7 +44,6 @@ func recalculateMinMax(someTM *TempManager, condition string, newTemp int) bool 
 				someTM.maxTemp = newTemp
 			}
 		} else {
-			fmt.Println(-1)
 			return false
 		}
 	case ">=":
@@ -89,12 +52,11 @@ func recalculateMinMax(someTM *TempManager, condition string, newTemp int) bool 
 				someTM.minTemp = newTemp
 			}
 		} else {
-			fmt.Println(-1)
 			return false
 		}
 	default:
-		fmt.Println(-1)
 		return false
 	}
+	someTM.optTemp = someTM.minTemp
 	return true
 }
