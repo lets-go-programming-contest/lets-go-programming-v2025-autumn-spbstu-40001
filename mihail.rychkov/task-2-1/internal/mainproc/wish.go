ada892c59796a98efc3e0993ca86d4d2b9613f3e
Richmick <ry4ckov.misha2018@yandex.ru>
2025-09-28 20:30:11 +0300

    [TASK-2-1] Separate wishes processing to package

--- mihail.rychkov/task-2-1/internal/mainproc/wish.go
+++ mihail.rychkov/task-2-1/internal/mainproc/wish.go
@@ -1,20 +1,22 @@
-import "errors"
+package mainproc;
+
+import "errors";
 
 type Wish struct {
 	minTemperature, maxTemperature int;
 }
-func (wish *Wish) includeMin(temperature int) {
+func (wish *Wish) IncludeMin(temperature int) {
 	if (wish.minTemperature < temperature) {
 		wish.minTemperature = temperature;
 	}
 }
-func (wish *Wish) includeMax(temperature int) {
+func (wish *Wish) IncludeMax(temperature int) {
 	if (wish.maxTemperature > temperature) {
-		wish.minTemperature = temperature;
+		wish.maxTemperature = temperature;
 	}
 }
 var ErrNoOptimum = errors.New("optimal temperature does not exist");
-func (wish *Wish) getOptimum() (int, error) {
+func (wish *Wish) GetOptimum() (int, error) {
 	if (wish.minTemperature > wish.maxTemperature) {
 		return 0, ErrNoOptimum;
 	}
