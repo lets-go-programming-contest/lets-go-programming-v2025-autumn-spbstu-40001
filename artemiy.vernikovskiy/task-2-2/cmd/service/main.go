c43d820271074fd670b8af283d29bb5f394e6d85
Aapng-cmd <verart1@yandex.ru>
2025-10-02 09:06:45 -0500

    [TASK-2-2] Initial

--- artemiy.vernikovskiy/task-2-2/cmd/service/main.go
+++ artemiy.vernikovskiy/task-2-2/cmd/service/main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"container/heap"
 )
 
 func main() {
+	var (
+		n int
+		
+	)
+	
+	_, err := fmt.Scan(&n)
+	if err != nil {
+		fmt.Println("Invalid dishes count")
+		return
+	}
+	
 	
 }
 
