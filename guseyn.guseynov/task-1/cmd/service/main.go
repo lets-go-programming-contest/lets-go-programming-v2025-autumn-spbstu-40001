e3e259522043bf713d01d1268bcf4786f7c4fd75
guseyn <ganx.gamer@gmail.com>
2025-10-02 21:32:11 +0300

    [TASK-1] add func printResult

--- guseyn.guseynov/task-1/cmd/service/main.go
+++ guseyn.guseynov/task-1/cmd/service/main.go
@@ -2,25 +2,45 @@ package main
 
 import "fmt"
 
+func printResult(operation string, op1, op2 int) {
+	switch operation {
+	case "+":
+		fmt.Println(op1 + op2)
+	case "*":
+		fmt.Println(op1 * op2)
+	case "-":
+		fmt.Println(op1 - op2)
+	case "/":
+		if op2 == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(op1 / op2)
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
+
 func main() {
 	var (
 		op1, op2  int
 		operation string
 	)
 
-	_, err1 := fmt.Scan(&op1)
-	if err1 != nil {
+	_, err := fmt.Scan(&op1)
+	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
-	_, err2 := fmt.Scan(&op2)
-	if err2 != nil {
+	_, err = fmt.Scan(&op2)
+	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
-	_, err3 := fmt.Scan(&operation)
-	if err3 != nil {
+	_, err = fmt.Scan(&operation)
+	if err != nil {
 		fmt.Println("Invalid operation")
 		return
 	}
+	printResult(operation, op1, op2)
 }
