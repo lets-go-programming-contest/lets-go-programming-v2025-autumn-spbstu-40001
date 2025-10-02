5b0b43557c2de1dd46272453ccfde32edda61cd2
guseyn <ganx.gamer@gmail.com>
2025-10-02 21:23:53 +0300

    [TASK-1] add input with checks

--- guseyn.guseynov/task-1/cmd/service/main.go
+++ guseyn.guseynov/task-1/cmd/service/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var (
+		op1, op2  int
+		operation string
+	)
+
+	_, err1 := fmt.Scan(&op1)
+	if err1 != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err2 := fmt.Scan(&op2)
+	if err2 != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	_, err3 := fmt.Scan(&operation)
+	if err3 != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
 }
