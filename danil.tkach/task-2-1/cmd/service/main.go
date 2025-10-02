5d785fd56e08563e7eaeb17a59a3e13afd4c80b9
Danil3352 <danil.tkach2018@yandex.ru>
2025-10-02 23:05:40 +0300

    [TASK-2-1] Add input

--- danil.tkach/task-2-1/cmd/service/main.go
+++ danil.tkach/task-2-1/cmd/service/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func processEmployee(currentMin, currentMax int) (int, int) {
+	var (
+		operation string
+		need_temp int
+	)
+	_, err := fmt.Scanf("%s %d", &operation, &need_temp)
+	if err != nil {
+		return 31, 14
+	}
+	return 0, 0
+}
+
+func main() {
+	var depart_count int
+	_, err := fmt.Scanln(&depart_count)
+	if err != nil {
+		return
+	}
+	//currentMin := 15
+	//currentMax := 30
+	for count := 0; count < depart_count; count++ {
+		var people_count int
+		_, err := fmt.Scanln(&people_count)
+		if err != nil {
+			return
+		}
+		for count2 := 0; count2 < people_count; count2++ {
+			processEmployee(0, 0)
+		}
+	}
+	return
 }
