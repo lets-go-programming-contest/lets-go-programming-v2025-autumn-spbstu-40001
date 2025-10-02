8c46c1d71c3b0fedaccb09a0a1877c7708aa554e
“Timofeev <maksimtmfv@gmail.com>
2025-10-02 15:09:15 +0300

    [TASK-2-1] modify main.go with program logic

--- maxim.timofeev/task-2-1/cmd/service/main.go
+++ maxim.timofeev/task-2-1/cmd/service/main.go
@@ -3,24 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	var N, K int
+	var N int
 	fmt.Scan(&N)
-	fmt.Scan(&K)
 
 	for i := 0; i < N; i++ {
+		var K int
+		fmt.Scan(&K)
 		temperatureRange := [2]int{15, 30}
 		for j := 0; j < K; j++ {
 			var direction string
 			var degrees int
 			fmt.Scan(&direction)
 			fmt.Scan(&degrees)
-			if direction == ">=" && (degrees >= temperatureRange[0] && degrees <= temperatureRange[1]) {
+			if direction == ">=" && (degrees <= temperatureRange[1]) {
 				temperatureRange[0] = degrees
-			} else if direction == "<=" && (degrees >= temperatureRange[0] && degrees <= temperatureRange[1]) {
+			} else if direction == "<=" && (degrees >= temperatureRange[0]) {
 				temperatureRange[1] = degrees
 			} else {
 				fmt.Println("-1")
+				break
 			}
+			fmt.Println(temperatureRange[0])
 		}
 	}
 }
