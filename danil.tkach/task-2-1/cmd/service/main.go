eefb3322126b143b57928b37c9dfbceb909df681
Danil3352 <danil.tkach2018@yandex.ru>
2025-10-03 23:45:40 +0300

    [TASK-2-1] Add all func logic

--- danil.tkach/task-2-1/cmd/service/main.go
+++ danil.tkach/task-2-1/cmd/service/main.go
@@ -10,7 +10,7 @@ func processEmployee(currentMin, currentMax int) (int, int) {
 		operation string
 		need_temp int
 	)
-	_, err := fmt.Scanf("%s %d", &operation, &need_temp)
+	_, err := fmt.Scanf("%s %d\n", &operation, &need_temp)
 	if err != nil {
 		return 31, 14
 	}
@@ -18,7 +18,7 @@ func processEmployee(currentMin, currentMax int) (int, int) {
 	case "<=":
 		currentMax = int(math.Min(float64(currentMax), float64(need_temp)))
 	case ">=":
-		currentMax = int(math.Max(float64(currentMin), float64(need_temp)))
+		currentMin = int(math.Max(float64(currentMin), float64(need_temp)))
 	default:
 		return 31, 14
 	}
@@ -31,17 +31,21 @@ func main() {
 	if err != nil {
 		return
 	}
-	//currentMin := 15
-	//currentMax := 30
 	for count := 0; count < depart_count; count++ {
+		currentMin := 15
+		currentMax := 30
 		var people_count int
 		_, err := fmt.Scanln(&people_count)
 		if err != nil {
 			return
 		}
 		for count2 := 0; count2 < people_count; count2++ {
-			processEmployee(0, 0)
+			currentMin, currentMax = processEmployee(currentMin, currentMax)
+			if currentMin <= currentMax && currentMax <= 30 && currentMin >= 15 {
+				fmt.Println(currentMin)
+			} else {
+				fmt.Println(-1)
+			}
 		}
 	}
-	return
 }
