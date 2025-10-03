9b94f60ca41ee4debff6e6493ce1a3cf736dfa25
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-10-03 15:13:34 +0300

    [TASK-2-1] add input reading

--- aleksey.maslov/task-2-1/cmd/service/main.go
+++ aleksey.maslov/task-2-1/cmd/service/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	var (
+		departmentCount int
+		employeesCount  int
+	)
+	_, err := fmt.Scan(&departmentCount)
+	if err != nil {
+		fmt.Println("Invalid department count")
+		return
+	}
+	for range departmentCount {
+		_, err = fmt.Scanln(&employeesCount)
+		if err != nil {
+			fmt.Println("Invalid employees count")
+			return
+		}
+	}
+}
