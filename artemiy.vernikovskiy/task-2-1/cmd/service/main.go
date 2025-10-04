eb00bef111f86a22d3b5e40d584e0502ab45fc20
Aapng-cmd <verart1@yandex.ru>
2025-10-04 10:33:29 -0500

    [TASK-2-1] Fixed linter

--- artemiy.vernikovskiy/task-2-1/cmd/service/main.go
+++ artemiy.vernikovskiy/task-2-1/cmd/service/main.go
@@ -6,35 +6,39 @@ import (
 
 func main() {
 	var (
-		nNumberOfOlimpic, kEmployeesCount             int
-		minTemp, maxTemp int
-		optimalTemp      int
-		border           string
+		nNumberOfOlimpic, kEmployeesCount int
+		minTemp, maxTemp                  int
+		optimalTemp                       int
+		border                            string
 	)
-	
+
 	_, err := fmt.Scan(&nNumberOfOlimpic)
 	if err != nil || nNumberOfOlimpic < 0 {
 		fmt.Println("Invalid departure count")
+
 		return
 	}
-	
+
 	for range nNumberOfOlimpic {
 		_, err = fmt.Scan(&kEmployeesCount)
 		if err != nil || kEmployeesCount < 0 {
-			fmt.Println("Invalid empolyees count")
+			fmt.Println("Invalid employees count")
+
 			return
 		}
-		
+
 		minTemp = 15
 		maxTemp = 30
 		optimalTemp = 0
-		
+
 		for range kEmployeesCount {
 			_, err = fmt.Scan(&border, &optimalTemp)
 			if err != nil {
 				fmt.Println("Invalid temperature")
+
 				return
 			}
+
 			switch border {
 			case ">=":
 				minTemp = max(minTemp, optimalTemp)
@@ -43,7 +47,7 @@ func main() {
 			default:
 				fmt.Println("Wrong operator")
 			}
-		
+
 			if maxTemp >= minTemp {
 				fmt.Println(minTemp)
 			} else {
@@ -52,4 +56,3 @@ func main() {
 		}
 	}
 }
-
