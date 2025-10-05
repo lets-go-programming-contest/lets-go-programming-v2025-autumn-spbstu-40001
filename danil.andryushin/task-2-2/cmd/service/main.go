4fdc5a1c510ed208a720dafde4378e4f397729ed
atroxxx <danilandriuschin@yandex.ru>
2025-10-05 20:08:40 +0300

    [TASK-2-2] aply linter --fix

--- danil.andryushin/task-2-2/cmd/service/main.go
+++ danil.andryushin/task-2-2/cmd/service/main.go
@@ -16,28 +16,38 @@ func main() {
 		nDishes int
 		dishes  = &extended_stack.Stack{Functor: Greater}
 	)
+
 	heap.Init(dishes)
+
 	_, err := fmt.Scan(&nDishes)
 	if err != nil || nDishes < 0 {
 		fmt.Println("invalid dishes count:")
+
 		return
 	}
+
 	var dishRating int
 	for range nDishes {
 		_, err = fmt.Scan(&dishRating)
 		if err != nil {
 			fmt.Println("wrong dish rating format:", err)
+
 			return
 		}
+
 		heap.Push(dishes, dishRating)
 	}
+
 	var dishNumber int
+
 	_, err = fmt.Scan(&dishNumber)
 	if err != nil || dishNumber < 0 || dishNumber > nDishes {
 		fmt.Println("invalid dish number")
 	}
+
 	for range dishNumber - 1 {
 		heap.Pop(dishes)
 	}
+
 	fmt.Println(heap.Pop(dishes))
 }
