36209febf51f78feb4770ad672281ebbe7e3045e
atroxxx <danilandriuschin@yandex.ru>
2025-10-05 20:08:40 +0300

    [TASK-2-2] aply linter --fix

--- danil.andryushin/task-2-2/internal/extended_stack/heap.go
+++ danil.andryushin/task-2-2/internal/extended_stack/heap.go
@@ -15,9 +15,11 @@ func (obj *Stack) Less(lhs, rhs int) bool {
 	if lhs >= obj.Len() || rhs >= obj.Len() {
 		panic("invalid index range")
 	}
+
 	if obj.Functor == nil {
 		return cmp.Less(obj.data[lhs], obj.data[rhs])
 	}
+
 	return obj.Functor(obj.data[lhs], obj.data[rhs])
 }
 
@@ -25,6 +27,7 @@ func (obj *Stack) Swap(lhs, rhs int) {
 	if lhs >= obj.Len() || rhs >= obj.Len() {
 		panic("invalid index range")
 	}
+
 	obj.data[lhs], obj.data[rhs] = obj.data[rhs], obj.data[lhs]
 }
 
@@ -33,6 +36,7 @@ func (obj *Stack) Push(data any) {
 	if !casted {
 		panic("unexpected data type (expected: int)")
 	}
+
 	obj.data = append(obj.data, iData)
 }
 
@@ -41,7 +45,9 @@ func (obj *Stack) Pop() any {
 	if size == 0 {
 		panic("heap underflow")
 	}
+
 	data := obj.data[size-1]
 	obj.data = obj.data[0 : size-1]
+
 	return data
 }
