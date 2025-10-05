f090a3b47eaa1dc083d54d81ba8655e6394b9130
“Timofeev <maksimtmfv@gmail.com>
2025-10-05 21:01:20 +0300

    [TASK-2-2] rework main with replace heap logic into her own file

--- maxim.timofeev/task-2-2/cmd/service/main.go
+++ maxim.timofeev/task-2-2/cmd/service/main.go
@@ -3,30 +3,9 @@ package main
 import (
 	"container/heap"
 	"fmt"
+
+	"github.com/PigoDog/task-2-2/package/container/intHeap"
 )
-
-type IntHeap []int
-
-func (h *IntHeap) Len() int           { return len(*h) }
-func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
-func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
-
-func (h *IntHeap) Push(x any) {
-	if val, check := x.(int); check {
-		*h = append(*h, val)
-	} else {
-		fmt.Println("invalid type in heap push")
-	}
-}
-
-func (h *IntHeap) Pop() any {
-	old := *h
-	length := len(old)
-	top := old[length-1]
-	*h = old[:length-1]
-
-	return top
-}
 
 func main() {
 	var (
@@ -54,7 +33,7 @@ func main() {
 		fmt.Println("Invalid input")
 	}
 
-	currentHeap := &IntHeap{}
+	currentHeap := &intHeap.IntHeap{}
 	heap.Init(currentHeap)
 
 	for currentDish := range dishCount {
