9db22d6546045d6c1ca232e95ba251f906b3067a
atroxxx <danilandriuschin@yandex.ru>
2025-10-05 19:19:10 +0300

    [TASK-2-2] implement heap

--- danil.andryushin/task-2-2/cmd/service/main.go
+++ danil.andryushin/task-2-2/cmd/service/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"container/heap"
+	"fmt"
+
+	"github.com/atroxxxxxx/task-2-2/internal/task"
+)
+
+func Greater(a, b int) bool {
+	return a > b
+}
 
 func main() {
-	fmt.Println("hello world")
+	test := &task.Heap{Functor: Greater}
+	heap.Init(test)
+	heap.Push(test, 3)
+	heap.Push(test, 5)
+	heap.Push(test, 7)
+	heap.Push(test, 9)
+	heap.Push(test, 2)
+	heap.Push(test, 6)
+	heap.Push(test, 12)
+	fmt.Println(*test)
+	heap.Pop(test)
+	fmt.Println(heap.Pop(test))
 }
