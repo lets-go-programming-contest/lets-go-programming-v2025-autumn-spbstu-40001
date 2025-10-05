92319cb96d83eaccc80638850978ae4ded5f0b28
DimasFantomasA <genapupin2000@gmail.com>
2025-10-05 20:22:08 +0300

    [TASK-1] Make final logic

--- trojan.dmitry/task-1/cmd/service/main.go
+++ trojan.dmitry/task-1/cmd/service/main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	println("Hello world")
+	var num1, num2 int
+	var operator string
+
+	_, err := fmt.Scan(&num1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scan(&num2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	_, err = fmt.Scan(&operator)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+	switch operator {
+	case "+":
+		fmt.Println(num1 + num2)
+	case "-":
+		fmt.Println(num1 - num2)
+	case "*":
+		fmt.Println(num1 * num2)
+	case "/":
+		if num2 == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(num1 / num2)
+		}
+	default:
+		fmt.Println("Invalid opeartion")
+	}
 }
