def683df240bafb2e2a4ed56b902f641861304a7
A1exCRE <alsmmm2065@gmail.com>
2025-10-07 02:33:45 +0300

    [TASK-1] Add input and verification

--- aleksandr.smirnov/task-1/cmd/service/main.go
+++ aleksandr.smirnov/task-1/cmd/service/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	var a int
+	var b int
+	var operation string
+	_, err := fmt.Scanln(&a)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scanln(&b)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	_, err = fmt.Scanln(&operation)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+}
