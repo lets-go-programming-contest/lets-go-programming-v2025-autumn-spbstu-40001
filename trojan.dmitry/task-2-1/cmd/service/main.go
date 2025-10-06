2a27b08661f0de3647f342c19641e64e795931ca
DimasFantomasA <genapupin2000@gmail.com>
2025-10-06 21:42:59 +0300

    [TASK-2-1] Make main logic

--- trojan.dmitry/task-2-1/cmd/service/main.go
+++ trojan.dmitry/task-2-1/cmd/service/main.go
@@ -1,1 +1,55 @@
-func main() {}
+package main
+
+import "fmt"
+
+func main() {
+	var department int
+
+	_, err := fmt.Scan(&department)
+	if err != nil {
+		fmt.Println("Invalid input")
+		return
+	}
+	for numOfDepts := 0; numOfDepts < department; numOfDepts++ {
+		var workers int
+		_, err = fmt.Scan(&workers)
+		if err != nil {
+			fmt.Println("Invalid input")
+			return
+		}
+		minLevel := 15
+		maxLevel := 30
+		for numOfEmployees := 0; numOfEmployees < workers; numOfEmployees++ {
+			var operator string
+			_, err = fmt.Scan(&operator)
+			if err != nil {
+				fmt.Println("Invalid input")
+				return
+			}
+			var num int
+			_, err = fmt.Scan(&num)
+			if err != nil {
+				fmt.Println("Invalid input")
+				return
+			}
+			if operator == ">=" {
+				if num > minLevel {
+					minLevel = num
+				}
+			} else if operator == "<=" {
+				if num < maxLevel {
+					maxLevel = num
+				}
+			} else {
+				fmt.Printf("Invalid input")
+				return
+			}
+
+			if minLevel <= maxLevel {
+				fmt.Println(minLevel)
+			} else {
+				fmt.Println(-1)
+			}
+		}
+	}
+}
