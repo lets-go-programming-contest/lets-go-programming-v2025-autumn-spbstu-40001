e19b0b3cf42a5bc6dc1f115522c526b6baf29a8c
DimasFantomasA <genapupin2000@gmail.com>
2025-10-06 22:02:41 +0300

    [TASK-2-1] Resolving linter problems

--- trojan.dmitry/task-2-1/cmd/service/main.go
+++ trojan.dmitry/task-2-1/cmd/service/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var department int
@@ -8,28 +10,32 @@ func main() {
 	_, err := fmt.Scan(&department)
 	if err != nil {
 		fmt.Println("Invalid input")
+
 		return
 	}
-	for numOfDepts := 0; numOfDepts < department; numOfDepts++ {
+	for range department {
 		var workers int
 		_, err = fmt.Scan(&workers)
 		if err != nil {
 			fmt.Println("Invalid input")
+
 			return
 		}
 		minLevel := 15
 		maxLevel := 30
-		for numOfEmployees := 0; numOfEmployees < workers; numOfEmployees++ {
+		for range workers {
 			var operator string
 			_, err = fmt.Scan(&operator)
 			if err != nil {
 				fmt.Println("Invalid input")
+
 				return
 			}
 			var num int
 			_, err = fmt.Scan(&num)
 			if err != nil {
 				fmt.Println("Invalid input")
+
 				return
 			}
 			switch operator {
@@ -43,6 +49,7 @@ func main() {
 				}
 			default:
 				fmt.Printf("Invalid input")
+
 				return
 			}
 
