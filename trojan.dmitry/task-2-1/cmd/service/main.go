01cd337a1704f9c3cd4133faff8bba783d065d8f
DimasFantomasA <genapupin2000@gmail.com>
2025-10-06 22:08:34 +0300

    [TASK-2-1] Fix lint

--- trojan.dmitry/task-2-1/cmd/service/main.go
+++ trojan.dmitry/task-2-1/cmd/service/main.go
@@ -13,31 +13,40 @@ func main() {
 
 		return
 	}
+
 	for range department {
 		var workers int
+
 		_, err = fmt.Scan(&workers)
 		if err != nil {
 			fmt.Println("Invalid input")
 
 			return
 		}
+
 		minLevel := 15
 		maxLevel := 30
+
 		for range workers {
 			var operator string
+
 			_, err = fmt.Scan(&operator)
 			if err != nil {
 				fmt.Println("Invalid input")
 
 				return
 			}
+
 			var num int
+
 			_, err = fmt.Scan(&num)
+
 			if err != nil {
 				fmt.Println("Invalid input")
 
 				return
 			}
+
 			switch operator {
 			case ">=":
 				if num > minLevel {
