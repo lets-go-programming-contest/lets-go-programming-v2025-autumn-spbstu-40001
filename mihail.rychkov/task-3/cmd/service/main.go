da0b32de71a15f6948dd249489cbb63f1e35db9c
Richmick <ry4ckov.misha2018@yandex.ru>
2025-10-07 15:08:31 +0300

    [TASK-3] Add argv[] parse

--- mihail.rychkov/task-3/cmd/service/main.go
+++ mihail.rychkov/task-3/cmd/service/main.go
@@ -1,3 +1,21 @@
 package main;
 
-func main() {}
+import "fmt";
+import "os";
+
+func main() {
+	args := os.Args;
+
+	if ((len(args) != 3) || (args[1] != "--config")) {
+		fmt.Println("missing --config <file> in cmd args");
+		return;
+	}
+
+	yamlFileData, err := os.ReadFile(args[2]);
+	if (err != nil) {
+		fmt.Println("failed to read config file:", err);
+		return;
+	}
+
+	fmt.Println(string(yamlFileData));
+}
