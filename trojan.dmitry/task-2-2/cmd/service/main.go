5e4118d24fa2ecd91903e5c7f6dfcadcb2985137
DimasFantomasA <genapupin2000@gmail.com>
2025-10-07 22:30:59 +0300

    [TASK-2-2] Attempts to fix linter

--- trojan.dmitry/task-2-2/cmd/service/main.go
+++ trojan.dmitry/task-2-2/cmd/service/main.go
@@ -7,21 +7,22 @@ import (
 
 type IntHeap []int
 
-func (iHeap IntHeap) Len() int {
-	return len(iHeap)
+func (iHeap *IntHeap) Len() int {
+	return len(*iHeap)
 }
 
-func (iHeap IntHeap) Less(firstIndex, secondIndex int) bool {
-	return iHeap[firstIndex] < iHeap[secondIndex]
+func (iHeap *IntHeap) Less(firstIndex, secondIndex int) bool {
+	return (*iHeap)[firstIndex] < (*iHeap)[secondIndex]
 }
 
-func (iHeap IntHeap) Swap(firstIndex, secondIndex int) {
-	iHeap[firstIndex], iHeap[secondIndex] = iHeap[secondIndex], iHeap[firstIndex]
+func (iHeap *IntHeap) Swap(firstIndex, secondIndex int) {
+	(*iHeap)[firstIndex], (*iHeap)[secondIndex] = (*iHeap)[secondIndex], (*iHeap)[firstIndex]
 }
 
 func (iHeap *IntHeap) Push(x any) {
 	value, ok := x.(int)
 	if !ok {
+		fmt.Println("Invalid type")
 
 		return
 	}
@@ -31,31 +32,40 @@ func (iHeap *IntHeap) Push(x any) {
 func (iHeap *IntHeap) Pop() any {
 	olhH := *iHeap
 	length := len(olhH)
-	first := olhH[length-1]
+	last := olhH[length-1]
 	*iHeap = olhH[:length-1]
 
-	return first
+	return last
+}
+
+func removeMinUntil(dishHeap *IntHeap, numOfPreference int) {
+	for dishHeap.Len() > numOfPreference {
+		heap.Pop(dishHeap)
+	}
 }
 
 func main() {
 	var countOfDishes int
 
 	_, err := fmt.Scan(&countOfDishes)
-	if err != nil || 10000 < countOfDishes || countOfDishes < 1 {
+	if err != nil || countOfDishes < 1 || countOfDishes > 10000 {
 		fmt.Println("Invalid input")
+
+		return
 	}
 
-	h := &IntHeap{}
-	heap.Init(h)
+	dishHeap := &IntHeap{}
+	heap.Init(dishHeap)
 
 	for range countOfDishes {
 		var rating int
 		_, err = fmt.Scan(&rating)
 		if err != nil || rating < -10000 || rating > 10000 {
 			fmt.Println("Invalid input")
+
 			return
 		}
-		heap.Push(h, rating)
+		heap.Push(dishHeap, rating)
 	}
 
 	var numOfPreference int
@@ -63,13 +73,14 @@ func main() {
 	_, err = fmt.Scan(&numOfPreference)
 	if err != nil || numOfPreference < 1 || numOfPreference > countOfDishes {
 		fmt.Println("Invalid input")
+
 		return
 	}
-	for h.Len() > numOfPreference {
-		heap.Pop(h)
-	}
-	if h.Len() == numOfPreference && h.Len() > 0 {
-		fmt.Println((*h)[0])
+
+	removeMinUntil(dishHeap, numOfPreference)
+
+	if dishHeap.Len() == numOfPreference && dishHeap.Len() > 0 {
+		fmt.Println((*dishHeap)[0])
 	} else {
 		fmt.Println("Invalid input")
 	}
