fde4ff6a6e6df5f25a7105a6c8f2dfbb639698ce
TimeouT_plz <danil.dolzhenkov1400@gmail.com>
2025-10-08 21:14:44 +0500

    [TASK-1] small fix

--- dolzhenkov.danil/task-1/cmd/service/main.go
+++ dolzhenkov.danil/task-1/cmd/service/main.go
@@ -10,28 +10,24 @@ func main() {
 		operation string
 	)
 
-	// Читаем первый операнд
 	_, err := fmt.Scan(&op1)
 	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
 
-	// Читаем второй операнд
 	_, err = fmt.Scan(&op2)
 	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
 
-	// Читаем операцию
 	_, err = fmt.Scan(&operation)
 	if err != nil {
 		fmt.Println("Invalid operation")
 		return
 	}
 
-	// Выполняем операцию
 	switch operation {
 	case "+":
 		fmt.Printf("%d\n", op1+op2)
