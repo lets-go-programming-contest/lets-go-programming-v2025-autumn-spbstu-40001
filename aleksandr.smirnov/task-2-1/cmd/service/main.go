c6a226c3c31cb7e796ccfd85a8c1670be0ae74a8
A1exCRE <alsmmm2065@gmail.com>
2025-10-09 21:57:13 +0300

    [TASK-2-1] Reading the number of departments

--- aleksandr.smirnov/task-2-1/cmd/service/main.go
+++ aleksandr.smirnov/task-2-1/cmd/service/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	var departmentCount int
+	_, err := fmt.Scanln(&departmentCount)
+	if err != nil {
+		fmt.Println("Invalid input", err)
+		return
+	}
+	fmt.Printf("Прочитано отделов: %d\n", departmentCount)
+}
