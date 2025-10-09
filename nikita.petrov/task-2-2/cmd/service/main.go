eed6dc8f907de735ec77624fa67c102cd2643928
Nekich06 <p.nikita.73526@gmail.com>
2025-10-09 23:09:18 +0300

    [TASK-2-2] Implemented solution using "container/heap" package

--- nikita.petrov/task-2-2/cmd/service/main.go
+++ nikita.petrov/task-2-2/cmd/service/main.go
@@ -1,3 +1,70 @@
 package main
 
-func main() {}
+import (
+	"container/heap"
+	"fmt"
+)
+
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] > h[j]
+}
+
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func main() {
+	var dishesNumber int
+
+	_, err := fmt.Scan(&dishesNumber)
+	if err != nil {
+		fmt.Println("Parse error")
+
+		return
+	}
+
+	ratingList := &IntHeap{}
+
+	for range dishesNumber {
+		var dishRating int
+		_, err = fmt.Scan(&dishRating)
+		if err != nil {
+			fmt.Println("Parse error")
+
+			return
+		}
+
+		heap.Push(ratingList, dishRating)
+	}
+
+	var wishedDish int
+
+	_, err = fmt.Scan(&wishedDish)
+	if err != nil {
+		fmt.Println("Parse error")
+	}
+
+	for range wishedDish - 1 {
+		heap.Pop(ratingList)
+	}
+
+	fmt.Println(heap.Pop(ratingList))
+}
