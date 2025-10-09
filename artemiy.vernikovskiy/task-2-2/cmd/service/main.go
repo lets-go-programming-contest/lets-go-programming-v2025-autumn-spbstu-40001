0c3aaa8380ef05a94b4fa164f6d6196518049985
Aapng-cmd <verart1@yandex.ru>
2025-10-09 09:49:48 -0500

    [TASK-2-2] Re-opened task

--- artemiy.vernikovskiy/task-2-2/cmd/service/main.go
+++ artemiy.vernikovskiy/task-2-2/cmd/service/main.go
@@ -1,10 +1,52 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
+
+	"github.com/Aapng-cmd/task-2-2/internal/my_heap"
 )
 
 func main() {
-	
-}
+	var (
+		nDishesNumber, actualDish, wanting int
+		workHeap                           = &my_heap.Heap{}
+	)
 
+	_, err := fmt.Scan(&nDishesNumber)
+	if err != nil {
+		fmt.Println("Invalid dishes count")
+
+		return
+	}
+
+	heap.Init(workHeap)
+
+	for range nDishesNumber {
+		_, err = fmt.Scan(&actualDish)
+		if err != nil {
+			fmt.Println("invalid input")
+
+			return
+		}
+
+		heap.Push(workHeap, actualDish)
+	}
+
+	_, err = fmt.Scan(&wanting)
+	if err != nil {
+		fmt.Println("invalid input")
+
+		return
+	}
+
+	for range wanting - 1 {
+		if workHeap.Len() == 0 {
+			fmt.Println("no dish for you")
+		}
+
+		heap.Pop(workHeap)
+	}
+
+	fmt.Println(heap.Pop(workHeap))
+}
--- artemiy.vernikovskiy/task-2-2/go.mod
+++ artemiy.vernikovskiy/task-2-2/go.mod
@@ -1,3 +1,3 @@
 module github.com/Aapng-cmd/task-2-2
 
-go 1.24.4
+go 1.22.7
