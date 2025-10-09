fad2f7522f7ba01e02f5a50436fc891b357b5f56
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-10-09 20:37:56 +0300

    [TASK-2-2] main structure

--- aleksey.maslov/task-2-2/cmd/service/main.go
+++ aleksey.maslov/task-2-2/cmd/service/main.go
@@ -1,3 +1,38 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	var dishesCount int
+
+	_, err := fmt.Scanln(&dishesCount)
+	if err != nil {
+		fmt.Println("Invalid input", err)
+
+		return
+	}
+	//create heap
+	for range dishesCount {
+		var rating int
+
+		_, err = fmt.Scanln(&rating)
+		if err != nil {
+			fmt.Println("Invalid input", err)
+
+			return
+		}
+		//push
+	}
+
+	var selectedDish int
+
+	_, err = fmt.Scanln(&selectedDish)
+	if err != nil {
+		fmt.Println("Invalid input", err)
+
+		return
+	}
+
+	result := //func
+		fmt.println(result)
+}
