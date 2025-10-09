4e63ced770e7e9f213fb514f04ca1cb058df6a33
Aapng-cmd <verart1@yandex.ru>
2025-10-09 09:49:48 -0500

    [TASK-2-2] Re-opened task

---
+++ artemiy.vernikovskiy/task-2-2/internal/my_heap/heap.go
@@ -0,0 +1,33 @@
+package my_heap
+
+type Heap []int
+
+func (heap *Heap) Len() int {
+	return len(*heap)
+}
+
+func (heap *Heap) Less(firstIndex, secondIndex int) bool {
+	return (*heap)[firstIndex] < (*heap)[secondIndex]
+}
+
+func (heap *Heap) Swap(firstIndex, secondIndex int) {
+	(*heap)[firstIndex], (*heap)[secondIndex] = (*heap)[secondIndex], (*heap)[firstIndex]
+}
+
+func (heap *Heap) Push(inter interface{}) {
+	number, ok := inter.(int)
+	if !ok {
+		return
+	}
+
+	*heap = append(*heap, number)
+}
+
+func (heap *Heap) Pop() any {
+	oldHeap := *heap
+	n := len(oldHeap)
+	x := oldHeap[n-1]
+	*heap = oldHeap[0 : n-1]
+
+	return x
+}
