a2259f74cc33ab0a0ca420e72402b0d6b99230d0
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-10-09 23:02:57 +0300

    [TASK-2-2] fixing lint

--- aleksey.maslov/task-2-2/internal/intheap/intheap.go
+++ aleksey.maslov/task-2-2/internal/intheap/intheap.go
@@ -4,16 +4,16 @@ import "fmt"
 
 type IntHeap []int
 
-func (h IntHeap) Len() int {
-	return len(h)
+func (h *IntHeap) Len() int {
+	return len(*h)
 }
 
-func (h IntHeap) Less(i, j int) bool {
-	return h[i] < h[j]
+func (h *IntHeap) Less(i, j int) bool {
+	return (*h)[i] < (*h)[j]
 }
 
-func (h IntHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
 func (h *IntHeap) Push(x any) {
@@ -34,5 +34,6 @@ func (h *IntHeap) Pop() any {
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
+
 	return x
 }
