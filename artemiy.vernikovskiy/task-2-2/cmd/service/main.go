cd4b322f6c123d3e998c8727f4130930bcc33977
Aapng-cmd <verart1@yandex.ru>
2025-10-09 09:17:27 -0500

    [TASK-2-2] First try of actual task

--- artemiy.vernikovskiy/task-2-2/cmd/service/main.go
+++ artemiy.vernikovskiy/task-2-2/cmd/service/main.go
@@ -1,22 +1,52 @@
 package main
 
 import (
-	"fmt"
 	"container/heap"
+	"fmt"
+
+	"github.com/Aapng-cmd/task-2-2/internal/my_heap"
 )
 
 func main() {
 	var (
-		n int
-		
+		n, a, wanting int
+		workHeap      = &my_heap.Heap{}
 	)
-	
+
 	_, err := fmt.Scan(&n)
 	if err != nil {
 		fmt.Println("Invalid dishes count")
+
 		return
 	}
-	
-	
-}
 
+	heap.Init(workHeap)
+
+	for range n {
+		_, err = fmt.Scan(&a)
+		if err != nil {
+			fmt.Println("invalid input")
+
+			return
+		}
+
+		heap.Push(workHeap, a)
+	}
+
+	_, err = fmt.Scan(&wanting)
+	if err != nil {
+		fmt.Println("invalid input")
+
+		return
+	}
+
+	for range wanting - 1 {
+		if workHeap.Len() == 0 {
+			fmt.Println("no dish for you")
+		}
+
+		heap.Pop(workHeap)
+	}
+
+	fmt.Println(heap.Pop(workHeap))
+}
