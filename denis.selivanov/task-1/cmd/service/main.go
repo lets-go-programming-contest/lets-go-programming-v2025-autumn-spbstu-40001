343a09875a9038485933f35c831b9675cc6b5a26
Chebupele <87374287+Chebupele@users.noreply.github.com>
2025-10-10 15:00:17 +0300

    [TASK-1] main logic implementation

--- denis.selivanov/task-1/cmd/service/main.go
+++ denis.selivanov/task-1/cmd/service/main.go
@@ -1,3 +1,55 @@
 package main
 
-func main() {}
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrInvalidOp    = errors.New("Invalid operation")
+	ErrDivideByZero = errors.New("Division by zero")
+)
+
+func evaluate(operand1, opearand2 int, op string) (int, error) {
+	switch op {
+	case "+":
+		return operand1 + opearand2, nil
+	case "-":
+		return operand1 - opearand2, nil
+	case "*":
+		return operand1 * opearand2, nil
+	case "/":
+		if opearand2 == 0 {
+			return 0, ErrDivideByZero
+		}
+		return operand1 / opearand2, nil
+	}
+	return 0, ErrInvalidOp
+}
+
+func main() {
+	var (
+		operand1, operand2 int
+		op                 string
+	)
+
+	_, err := fmt.Scan(&operand1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+	}
+	_, err = fmt.Scan(&operand2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+	}
+	_, err = fmt.Scan(&op)
+	if err != nil {
+		fmt.Println("Invalid operation")
+	}
+
+	result, err := evaluate(operand1, operand2, op)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(result)
+}
