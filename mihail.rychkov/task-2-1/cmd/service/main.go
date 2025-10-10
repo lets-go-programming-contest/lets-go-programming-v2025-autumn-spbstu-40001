ccb3f9bbed6e4687b43a1fcf7956133e2a1ad453
Richmick <ry4ckov.misha2018@yandex.ru>
2025-10-10 08:49:40 +0300

    [TASK-2-1] Squash main logic into main()

--- mihail.rychkov/task-2-1/cmd/service/main.go
+++ mihail.rychkov/task-2-1/cmd/service/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io"
-	"os"
 
 	"github.com/Rychmick/task-2-1/internal/wish"
 )
@@ -21,66 +19,52 @@ func main() {
 
 	_, err := fmt.Scan(&nDepartments)
 	if err != nil {
-		fmt.Println("Failed to read departments count", err)
+		fmt.Println("failed to read departments count:", err)
 
 		return
 	}
 
 	for range nDepartments {
-		_, err := ProcessDepartmentWishes(os.Stdin, os.Stdout)
+		var nWishes uint
+
+		_, err = fmt.Scan(&nWishes)
 		if err != nil {
-			fmt.Println(err)
-		}
-	}
-}
+			fmt.Println("failed to scan wishes count:", err)
 
-func ProcessDepartmentWishes(istream io.Reader, logstream io.Writer) (wish.Wish, error) {
-	var (
-		nWishes    uint
-		commonWish wish.Wish
-	)
-
-	commonWish.Init(minSupportedTemperature, maxSupportedTemperature)
-
-	_, err := fmt.Fscan(istream, &nWishes)
-	if err != nil {
-		return commonWish, fmt.Errorf("failed to scan wishes count: %w", err)
-	}
-
-	for range nWishes {
-		err = ProcessWish(istream, &commonWish)
-		if err != nil {
-			return commonWish, err
+			return
 		}
 
-		if logstream != nil {
+		commonWish := wish.New(minSupportedTemperature, maxSupportedTemperature)
+
+		for range nWishes {
+			err = ProcessWishFromStdin(&commonWish)
+			if err != nil {
+				fmt.Println("failed to process wish:", err)
+
+				return
+			}
+
 			temperature, err := commonWish.GetOptimum()
 			if err != nil {
-				_, err = fmt.Fprintln(logstream, -1)
+				fmt.Println(-1)
 			} else {
-				_, err = fmt.Fprintln(logstream, temperature)
-			}
-
-			if err != nil {
-				return commonWish, fmt.Errorf("failed to print log: %w", err)
+				fmt.Println(temperature)
 			}
 		}
 	}
-
-	return commonWish, nil
 }
 
-func ProcessWish(istream io.Reader, wish *wish.Wish) error {
+func ProcessWishFromStdin(wish *wish.Wish) error {
 	var (
 		sign        string
 		temperature int
 	)
 
-	_, err := fmt.Fscan(istream, &sign, &temperature)
+	_, err := fmt.Scan(&sign, &temperature)
 
 	switch {
 	case err != nil:
-		return fmt.Errorf("failed to process wish: %w", err)
+		return fmt.Errorf("cannot read wish: %w", err)
 	case sign == ">=":
 		wish.IncludeMin(temperature)
 
