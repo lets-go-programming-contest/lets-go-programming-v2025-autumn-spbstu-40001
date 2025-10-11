cc6c0c9a83e4b24f4d3c59cf49238d50e3e781a8
lee <likachich12@gmail.com>
2025-10-11 18:08:05 +0300

    [TASK-2-1] main go add

--- hismatova.elizaveta/task-2-1/cmd/service/main.go
+++ hismatova.elizaveta/task-2-1/cmd/service/main.go
@@ -1,2 +1,72 @@
 package main
-func main() {}
+
+import "fmt"
+
+const (
+	MinTemp = 15
+	MaxTemp = 30
+)
+
+func UpdateRangeAndGetTemp(
+	minT, maxT int,
+	operation string,
+	temp int,
+) (int, int) {
+	switch operation {
+	case "<=":
+		if temp < maxT {
+			maxT = temp
+		}
+	case ">=":
+		if temp > minT {
+			minT = temp
+		}
+	}
+	if minT > maxT {
+		return -1, maxT
+	}
+	return minT, maxT
+}
+
+func main() {
+	var departCount int
+	if _, err := fmt.Scanln(&departCount); err != nil {
+		fmt.Println("Error reading number of departments:", err)
+		return
+	}
+	for i := 0; i < departCount; i++ {
+		var peopleCount int
+		if _, err := fmt.Scanln(&peopleCount); err != nil {
+			fmt.Println("Error reading number of people:", err)
+			return
+		}
+		minT, maxT := MinTemp, MaxTemp
+		for j := 0; j < peopleCount; j++ {
+			var (
+				operation string
+				needTemp  int
+			)
+			if _, err := fmt.Scanf("%s %d\n", &operation, &needTemp); err != nil {
+				fmt.Println("Error reading operation and temperature:", err)
+				return
+			}
+
+			switch operation {
+			case "<=":
+				if needTemp < maxT {
+					maxT = needTemp
+				}
+			case ">=":
+				if needTemp > minT {
+ 					minT = needTemp
+				}
+			}
+
+			if minT > maxT {
+				fmt.Println(-1)
+			} else {
+				fmt.Println(minT)
+			}
+		}
+	}
+}
