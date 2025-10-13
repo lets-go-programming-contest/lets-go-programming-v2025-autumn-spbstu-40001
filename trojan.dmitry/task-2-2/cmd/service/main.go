fe8cbe1b52a4cdc3d2ff6cebd9f362b5177d5320
DimasFantomasA <genapupin2000@gmail.com>
2025-10-13 21:02:34 +0300

    [TASK-2-2] Trying to simplify main

--- trojan.dmitry/task-2-2/cmd/service/main.go
+++ trojan.dmitry/task-2-2/cmd/service/main.go
@@ -24,101 +24,124 @@ func (iHeap *IntHeap) Push(x any) {
 	if !ok {
 		panic("heap: Push of non-int value")
 	}
-
 	*iHeap = append(*iHeap, value)
 }
 
 func (iHeap *IntHeap) Pop() any {
-	olhH := *iHeap
-	length := len(olhH)
+	oldH := *iHeap
+	n := len(oldH)
 
-	if length == 0 {
-
+	if n == 0 {
 		return nil
 	}
-	last := olhH[length-1]
-	*iHeap = olhH[:length-1]
+
+	last := oldH[n-1]
+	*iHeap = oldH[:n-1]
 
 	return last
 }
 
-func removeMinUntil(dishHeap *IntHeap, numOfPreference int) {
-	for dishHeap.Len() > numOfPreference {
-		heap.Pop(dishHeap)
+func removeMinUntil(dh *IntHeap, k int) {
+	for dh.Len() > k {
+		heap.Pop(dh)
 	}
 }
 
 func validateCountOfDishes(count int) bool {
 	if count < 1 || count > 10000 {
 		fmt.Println("Count of dishes out of allowed range")
+
 		return false
 	}
+
 	return true
 }
-func main() {
-	var countOfDishes int
 
-	_, err := fmt.Scan(&countOfDishes)
+func readCountOfDishes() (int, error) {
+	var count int
+	_, err := fmt.Scan(&count)
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func readRatingsToHeap(count int) (*IntHeap, error) {
+	h := &IntHeap{}
+	heap.Init(h)
+
+	for i := 0; i < count; i++ {
+		var rating int
+		_, err := fmt.Scan(&rating)
+		if err != nil {
+			return nil, err
+		}
+		heap.Push(h, rating)
+	}
+
+	return h, nil
+}
+
+func readNumOfPreference(max int) (int, error) {
+	var k int
+	_, err := fmt.Scan(&k)
+	if err != nil {
+		return 0, err
+	}
+	if k < 1 || k > max {
+		return 0, fmt.Errorf("num of preference out of allowed range")
+	}
+	return k, nil
+}
+
+func main() {
+	countOfDishes, err := readCountOfDishes()
 	if err != nil {
 		fmt.Println("Invalid input of count of dishes")
 
 		return
 	}
+
 	if !validateCountOfDishes(countOfDishes) {
+		return
+	}
+
+	dishHeap, err := readRatingsToHeap(countOfDishes)
+	if err != nil {
+		fmt.Println("Invalid input of rating of dish")
 
 		return
 	}
 
-	dishHeap := &IntHeap{}
-	heap.Init(dishHeap)
-
-	for range countOfDishes {
-		var rating int
-
-		_, err = fmt.Scan(&rating)
-		if err != nil {
-			fmt.Println("Invalid input of rating of dish")
-
-			return
-		}
-
-		heap.Push(dishHeap, rating)
-	}
-
-	var numOfPreference int
-
-	_, err = fmt.Scan(&numOfPreference)
+	numOfPreference, err := readNumOfPreference(countOfDishes)
 	if err != nil {
 		fmt.Println("Invalid input of num of preference")
 
 		return
 	}
 
-	if numOfPreference < 1 || numOfPreference > countOfDishes {
-		fmt.Println("Num of preference out of allowed range")
+	removeMinUntil(dishHeap, numOfPreference)
+
+	if dishHeap.Len() != numOfPreference || dishHeap.Len() == 0 {
+		fmt.Println("Heap size mismatch after trimming")
 
 		return
 	}
 
-	removeMinUntil(dishHeap, numOfPreference)
+	val := heap.Pop(dishHeap)
+	if val == nil {
+		fmt.Println("Unexpected nil from heap.Pop")
 
-	if dishHeap.Len() == numOfPreference && dishHeap.Len() > 0 {
-		val := heap.Pop(dishHeap)
-		if val == nil {
-			fmt.Println("Unexpected nil from heap.Pop")
-
-			return
-		}
-		got, ok := val.(int)
-
-		if !ok {
-			fmt.Println("Heap returned non-int value")
-
-			return
-		}
-
-		fmt.Println(got)
-	} else {
-		fmt.Println("Heap size mismatch after trimming")
+		return
 	}
+
+	got, ok := val.(int)
+	if !ok {
+		fmt.Println("Heap returned non-int value")
+
+		return
+	}
+
+	fmt.Println(got)
 }
