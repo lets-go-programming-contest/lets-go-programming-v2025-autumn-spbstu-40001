f5565f5942386f563dd0eaa3b71609a695061a1a
A1exCRE <alsmmm2065@gmail.com>
2025-10-13 23:37:53 +0300

    [TASK-2-2] Fix lint

--- aleksandr.smirnov/task-2-2/internal/intheap/intheap.go
+++ aleksandr.smirnov/task-2-2/internal/intheap/intheap.go
@@ -2,16 +2,16 @@ package intheap
 
 type IntHeap []int
 
-func (h IntHeap) Len() int {
-	return len(h)
+func (h *IntHeap) Len() int {
+	return len(*h)
 }
 
-func (h IntHeap) Less(i, j int) bool {
-	return h[i] > h[j]
+func (h *IntHeap) Less(i, j int) bool {
+	return (*h)[i] > (*h)[j]
 }
 
-func (h IntHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
 func (h *IntHeap) Push(x interface{}) {
@@ -25,14 +25,14 @@ func (h *IntHeap) Push(x interface{}) {
 
 func (h *IntHeap) Pop() interface{} {
 	old := *h
-	n := len(old)
+	length := len(old)
 
-	if n == 0 {
+	if length == 0 {
 		return nil
 	}
 
-	x := old[n-1]
-	*h = old[0 : n-1]
+	x := old[length-1]
+	*h = old[0 : length-1]
 
 	return x
 }
