1f6ee26eeac2045786f4d8488b3e1899329cd875
A1exCRE <alsmmm2065@gmail.com>
2025-10-13 23:09:47 +0300

    [TASK-2-2] Implement main program logic with input

--- aleksandr.smirnov/task-2-2/cmd/service/main.go
+++ aleksandr.smirnov/task-2-2/cmd/service/main.go
@@ -1,3 +1,41 @@
 package main
 
-func main() {}
+import (
+	"container/heap"
+	"fmt"
+
+	"github.com/A1exCRE/task-2-2/internal/intheap"
+)
+
+func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Error:", r)
+		}
+	}()
+
+	var dishCount int
+
+	_, err := fmt.Scan(&dishCount)
+	if err != nil {
+		fmt.Println("Invalid input", err)
+		return
+	}
+
+	ratingsHeap := &intheap.IntHeap{}
+	heap.Init(ratingsHeap)
+
+	for range dishCount {
+		var rating int
+
+		_, err := fmt.Scan(&rating)
+		if err != nil {
+			fmt.Println("Invalid input")
+			return
+		}
+
+		heap.Push(ratingsHeap, rating)
+	}
+
+	fmt.Println("Куча:", *ratingsHeap)
+}
