226ec18d771feeacdb5633622c8b910f59534042
AlexeyFinaev02 <finaev0212212@mail.ru>
2025-10-13 19:53:40 +0300

    [TASK-2-1] add structure ComfortTemperature

--- finaev.alexey/task-2-1/cmd/service/main.go
+++ finaev.alexey/task-2-1/cmd/service/main.go
@@ -4,63 +4,70 @@ import (
 	"fmt"
 )
 
-func main() {
-	var (
-		grade    int
-		sign     string
-		emploees int
-		depart   int
-	)
+type ComfortTemperature struct {
+	minT int
+	maxT int
+}
 
-	_, err := fmt.Scan(&depart)
-	if err != nil {
+func (temperature *ComfortTemperature) printTemperature(sign string, grade int) {
+	switch sign {
+	case "<=":
+		temperature.maxT = min(temperature.maxT, grade)
+	case ">=":
+		temperature.minT = max(temperature.minT, grade)
+	default:
 		fmt.Println("Invalid input")
 
 		return
 	}
 
+	if temperature.minT > temperature.maxT {
+		fmt.Println(-1)
+	} else {
+		fmt.Println(temperature.minT)
+	}
+}
+
+func main() {
+	var depart int
+
+	_, err := fmt.Scan(&depart)
+	if err != nil {
+		fmt.Println("Invalid:", err)
+
+		return
+	}
+
 	for range depart {
+		var (
+			emploees    int
+			temperature = ComfortTemperature{15, 30}
+		)
 		_, err = fmt.Scan(&emploees)
 		if err != nil {
-			fmt.Println("Invalid input")
+			fmt.Println("Invalid:", err)
 
 			return
 		}
 
-		maxT := 30
-		minT := 15
-
 		for range emploees {
+			var (
+				grade int
+				sign  string
+			)
 			_, err = fmt.Scan(&sign)
 			if err != nil {
-				fmt.Println("Invalid input")
+				fmt.Println("Invalid:", err)
 
 				return
 			}
-
 			_, err = fmt.Scan(&grade)
 			if err != nil {
-				fmt.Println("Invalid input")
+				fmt.Println("Invalid:", err)
 
 				return
 			}
-
-			switch sign {
-			case "<=":
-				maxT = min(maxT, grade)
-			case ">=":
-				minT = max(minT, grade)
-			default:
-				fmt.Println("Invalid input")
-
-				return
-			}
-
-			if minT > maxT {
-				fmt.Println(-1)
-			} else {
-				fmt.Println(minT)
-			}
+			temperature.printTemperature(sign, grade)
 		}
 	}
 }
