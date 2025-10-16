df0689f58c125bcb09bca6f114cb62c1bf0145f5
guseyn <ganx.gamer@gmail.com>
2025-10-17 01:40:43 +0300

    [TASK-2-2] add intheap and complete main

--- guseyn.guseynov/task-2-2/cmd/main.go
+++ guseyn.guseynov/task-2-2/cmd/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"container/heap"
+	"fmt"
 
+	"github.com/GuseynovGuseynGG/task-2-2/internal/intheap"
+)
+
+func main() {
+	var dishesCount int
+	if _, err := fmt.Scan(&dishesCount); err != nil {
+		fmt.Println("Invalid input")
+		return
+	}
+
+	ratings := &intheap.IntHeap{}
+	heap.Init(ratings)
+
+	for i := 0; i < dishesCount; i++ {
+		var rating int
+		if _, err := fmt.Scan(&rating); err != nil {
+			fmt.Println("Invalid input")
+			return
+		}
+		heap.Push(ratings, rating)
+	}
+
+	var selectedDish int
+	if _, err := fmt.Scan(&selectedDish); err != nil {
+		fmt.Println("Invalid input")
+		return
+	}
+
+	if selectedDish > dishesCount || selectedDish <= 0 {
+		fmt.Println("There is no such dish")
+		return
+	}
+
+	for i := 0; i < selectedDish-1; i++ {
+		heap.Pop(ratings)
+	}
+
+	fmt.Println(heap.Pop(ratings))
 }
