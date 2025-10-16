faa58cc62d23ce6951ede9460701fdb972850bef
Vika <vikagluskova2006@gmail.com>
2025-10-17 02:39:02 +0300

    [TASK-2-2] solution with heap implementation

--- victoria.glushkova/task-2-2/cmd/service/main.go
+++ victoria.glushkova/task-2-2/cmd/service/main.go
@@ -25,19 +25,24 @@ func (h *DishRatingHeap) Push(x interface{}) {
 	rating, ok := x.(int)
 	if !ok {
 		fmt.Println("Error: expected integer value")
+
 		return
 	}
+
 	h.ratings = append(h.ratings, rating)
 }
 
 func (h *DishRatingHeap) Pop() interface{} {
 	if len(h.ratings) == 0 {
 		fmt.Println("Error: cannot pop from empty heap")
+
 		return nil
 	}
+
 	n := len(h.ratings)
 	item := h.ratings[n-1]
 	h.ratings = h.ratings[:n-1]
+
 	return item
 }
 
@@ -47,11 +52,13 @@ func main() {
 	_, err := fmt.Scan(&totalDishes)
 	if err != nil {
 		fmt.Println("Error reading number of dishes:", err)
+
 		return
 	}
 
 	if totalDishes < 1 || totalDishes > 10000 {
 		fmt.Println("Error: number of dishes must be between 1 and 10000")
+
 		return
 	}
 
@@ -63,13 +70,16 @@ func main() {
 	for range totalDishes {
 		var currentRating int
 		_, err := fmt.Scan(&currentRating)
+
 		if err != nil {
 			fmt.Println("Error reading dish rating:", err)
+
 			return
 		}
 
 		if currentRating < -10000 || currentRating > 10000 {
 			fmt.Println("Error: rating must be between -10000 and 10000")
+
 			return
 		}
 
@@ -81,11 +91,13 @@ func main() {
 	_, err = fmt.Scan(&preferenceOrder)
 	if err != nil {
 		fmt.Println("Error reading preference order:", err)
+
 		return
 	}
 
 	if preferenceOrder < 1 || preferenceOrder > totalDishes {
 		fmt.Printf("Error: preference order must be between 1 and %d\n", totalDishes)
+
 		return
 	}
 
@@ -104,12 +116,14 @@ func main() {
 		item := heap.Pop(tempHeap)
 		if item == nil {
 			fmt.Println("Error: not enough elements in heap")
+
 			return
 		}
 
 		rating, ok := item.(int)
 		if !ok {
 			fmt.Println("Error: unexpected type in heap")
+
 			return
 		}
 
