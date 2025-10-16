4943af1823a3e9eb5cde6edeedd70f8823c88542
shabunkov2155@mail.ru <shabunkov2155@mail.ru>
2025-10-17 00:34:36 +0300

    [TASK-2-1] initial commit for task-2-1

---
+++ kirill.shabunkov/task-2-1/cmd/service/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Temperature struct {
+	Min int
+	Max int
+}
+
+const (
+	minTemperature = 15
+	maxTemperature = 30
+)
+
+func (temp *Temperature) getSuitableTemperature(operand string, prefferedTemperature int) {
+	switch operand {
+	case ">=":
+		temp.Min = max(temp.Min, prefferedTemperature)
+	case "<=":
+		temp.Max = min(temp.Max, prefferedTemperature)
+	default:
+		fmt.Println("Wrong operand!")
+
+		return
+	}
+
+	if temp.Min > temp.Max {
+		fmt.Println(-1)
+	} else {
+		fmt.Println(temp.Min)
+	}
+}
+
+func main() {
+	var departamentNum int
+
+	_, err := fmt.Scan(&departamentNum)
+	if err != nil {
+		fmt.Println("Invalid input data: ", err)
+
+		return
+	}
+
+	for range departamentNum {
+		var workerNum int
+		currentTemperature := Temperature{Min: minTemperature, Max: maxTemperature}
+
+		_, err := fmt.Scan(&workerNum)
+		if err != nil {
+			fmt.Println("Invalid input data: ", err)
+
+			return
+		}
+
+		for range workerNum {
+			var (
+				prefferedTemperature int
+				operand              string
+			)
+
+			count, err := fmt.Scan(&operand, &prefferedTemperature)
+			if count != 2 {
+				fmt.Println("Incorrect amount of input data!")
+
+				return
+			}
+			if err != nil {
+				fmt.Println("Invalid input data: ", err)
+
+				return
+			}
+
+			currentTemperature.getSuitableTemperature(operand, prefferedTemperature)
+		}
+	}
+}
