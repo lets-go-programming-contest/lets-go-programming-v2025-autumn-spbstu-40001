5b63125c67b89af6c6271e38a03033d2a3164a90
Nekich06 <p.nikita.73526@gmail.com>
2025-10-16 23:45:17 +0300

    [TASK-3] Implemented parsing of YAML

--- nikita.petrov/task-3/cmd/service/main.go
+++ nikita.petrov/task-3/cmd/service/main.go
@@ -1,3 +1,40 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	InputFile  string `yaml:"input-file"`
+	OutputFile string `yaml:"output-file"`
+}
+
+var configPathFlag = flag.String("config", "config.yaml", "path to config file")
+
+func main() {
+	configFile, err := os.Open(*configPathFlag)
+	if err != nil && os.IsNotExist(err) {
+		panic("config file does not exist")
+	}
+
+	configData, err := io.ReadAll(configFile)
+	if err != nil {
+		panic("cannot read file")
+	}
+
+	var files Config
+
+	err = yaml.Unmarshal([]byte(configData), &files)
+
+	if err != nil {
+		panic("cannot unmarshal data")
+	}
+
+	fmt.Println(files.InputFile)
+	fmt.Println(files.OutputFile)
+}
