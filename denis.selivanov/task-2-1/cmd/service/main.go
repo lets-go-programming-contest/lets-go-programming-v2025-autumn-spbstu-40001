5420feb04d9e291846fa9dd67428aff4e8efaeb9
Chebupele <87374287+Chebupele@users.noreply.github.com>
2025-10-16 21:01:45 +0300

    [TASK-2-1] main logic implementation

--- denis.selivanov/task-2-1/cmd/service/main.go
+++ denis.selivanov/task-2-1/cmd/service/main.go
@@ -1,7 +1,90 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+type ComfortTemperature struct {
+	minTemp int
+	maxTemp int
+}
+
+var ErrInvalidSign error = errors.New("invalid sign")
+
+func (temperature *ComfortTemperature) UpdateTemperature(sign string, value int) error {
+	switch sign {
+	case "<=":
+		temperature.maxTemp = min(temperature.maxTemp, value)
+	case ">=":
+		temperature.minTemp = max(temperature.minTemp, value)
+	default:
+		return ErrInvalidSign
+	}
+
+	return nil
+}
+
+var ErrInvalidTemperature error = errors.New("invalid temperature")
+
+func (temperature *ComfortTemperature) GetTemperature() (int, error) {
+	if temperature.maxTemp < temperature.minTemp {
+		return 0, ErrInvalidTemperature
+	}
+
+	return temperature.minTemp, nil
+}
+
+const minTemp, maxTemp, invalidTemp int = 15, 30, -1
 
 func main() {
-	fmt.Println("TASK-2-1")
+	var departmentsCount uint
+
+	_, err := fmt.Scan(&departmentsCount)
+	if err != nil {
+		fmt.Println("invalid departments count:", err)
+
+		return
+	}
+
+	for range departmentsCount {
+		var employeesCount int
+
+		_, err = fmt.Scan(&employeesCount)
+		if err != nil {
+			fmt.Println("invalid employee count:", err)
+
+			return
+		}
+
+		temperature := ComfortTemperature{minTemp, maxTemp}
+
+		for range employeesCount {
+			var (
+				conditionSign  string
+				conditionValue int
+			)
+
+			_, err = fmt.Scan(&conditionSign, &conditionValue)
+			if err != nil {
+				fmt.Println("invalid temperature value:", err)
+
+				return
+			}
+
+			err = temperature.UpdateTemperature(conditionSign, conditionValue)
+			if err != nil {
+				fmt.Println("failed to update temperature:", err)
+
+				return
+			}
+
+			currentTemperature, err := temperature.GetTemperature()
+			if err != nil {
+				fmt.Println(invalidTemp)
+			} else {
+				fmt.Println(currentTemperature)
+			}
+		}
+	}
 }
