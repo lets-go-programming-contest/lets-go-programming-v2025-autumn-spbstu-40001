755915bbfda6da8c7cbc0cc99925b1ac00bcd3d7
guseyn <ganx.gamer@gmail.com>
2025-10-17 00:17:36 +0300

    [TASK-2-1] add min and max utility functions

---
+++ guseyn.guseynov/task-2-1/internal/controller/temperature.go
@@ -0,0 +1,48 @@
+package controller
+
+import "errors"
+
+var ErrInvalidTemp = errors.New("invalid temperature")
+var ErrUnknownOperator = errors.New("unknown operator")
+
+type TemperatureController struct {
+	minTemperature, maxTemperature int
+}
+
+type Desire struct {
+	Sign               string
+	DesiredTemperature int
+}
+
+func New(minTemperature, maxTemperature int) TemperatureController {
+	return TemperatureController{minTemperature, maxTemperature}
+}
+
+func (obj *TemperatureController) ChangeTemperature(desire Desire) error {
+	switch desire.Sign {
+	case ">=":
+		obj.updateMinTemperature(desire.DesiredTemperature)
+	case "<=":
+		obj.updateMaxTemperature(desire.DesiredTemperature)
+	default:
+		return ErrUnknownOperator
+	}
+
+	return nil
+}
+
+func (obj *TemperatureController) GetTemperature() (int, error) {
+	if obj.maxTemperature >= obj.minTemperature {
+		return obj.minTemperature, nil
+	}
+
+	return 0, ErrInvalidTemp
+}
+
+func (obj *TemperatureController) updateMaxTemperature(temp int) {
+	obj.maxTemperature = min(obj.maxTemperature, temp)
+}
+
+func (obj *TemperatureController) updateMinTemperature(temp int) {
+	obj.minTemperature = max(obj.minTemperature, temp)
+}
