dbad9d96f63c4f54dbd8eaec10652dcf34cd5cd0
guseyn <ganx.gamer@gmail.com>
2025-10-17 00:29:02 +0300

    [TASK-2-1] upgrade main.go

--- guseyn.guseynov/task-2-1/cmd/service/main.go
+++ guseyn.guseynov/task-2-1/cmd/service/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+const (
+	minTemp, maxTemp = 15, 30
+)
+
+func main() {
+	var nDepartments uint
+
+	_, err := fmt.Scan(&nDepartments)
+	if err != nil {
+		fmt.Println("invalid departments count:", err)
+
+		return
+	}
+
+	for range nDepartments {
+		fmt.Println("Processing department...")
+	}
 }
