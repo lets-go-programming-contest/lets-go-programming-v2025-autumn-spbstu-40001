f7583f092c4db6606952c81f9522b02796339307
Chebupele <87374287+Chebupele@users.noreply.github.com>
2025-10-16 23:49:36 +0300

    [TASK-2-2] minor logic rework + added erroadded error handling

--- denis.selivanov/task-2-2/cmd/service/main.go
+++ denis.selivanov/task-2-2/cmd/service/main.go
@@ -12,51 +12,85 @@ type Dish struct {
 
 type DishHeap []Dish
 
-func (h DishHeap) Len() int { return len(h) }
+func (h *DishHeap) Len() int { return len(*h) }
 
-func (h DishHeap) Less(i, j int) bool {
-	return h[i].rating > h[j].rating
+func (h *DishHeap) Less(i, j int) bool {
+	return (*h)[i].rating > (*h)[j].rating
 }
 
-func (h DishHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+func (h *DishHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
-func (h *DishHeap) Push(x interface{}) {
-	*h = append(*h, x.(Dish))
+func (h *DishHeap) Push(x any) {
+	dish, ok := x.(Dish)
+	if !ok {
+		panic("invalid data type")
+	}
+	*h = append(*h, dish)
 }
 
-func (h *DishHeap) Pop() interface{} {
+func (h *DishHeap) Pop() any {
 	old := *h
 	n := len(old)
+	if n == 0 {
+		panic("heap is empty")
+	}
 	x := old[n-1]
 	*h = old[0 : n-1]
 	return x
 }
 
 func main() {
-	var n, k int
-	fmt.Scanf("%d", &n)
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("Panic recovered:", err)
+		}
+	}()
 
-	ratings := make([]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Scanf("%d", &ratings[i])
+	var numberDishes, dishCount int
+	_, err := fmt.Scan(&numberDishes)
+	if err != nil {
+		fmt.Println("Invalid input:", err)
+		return
 	}
 
-	fmt.Scanf("%d", &k)
+	if numberDishes <= 0 {
+		fmt.Println("Number of dishes must be greater than 0.")
+		return
+	}
+
+	ratings := make([]int, numberDishes)
+	for i := 0; i < numberDishes; i++ {
+		_, err = fmt.Scan(&ratings[i])
+		if err != nil {
+			fmt.Println("Invalid input:", err)
+			return
+		}
+	}
+
+	_, err = fmt.Scan(&dishCount)
+	if err != nil {
+		fmt.Println("Invalid input:", err)
+		return
+	}
+
+	if dishCount <= 0 || dishCount > numberDishes {
+		fmt.Println("Dish count must be between 1 and", numberDishes)
+		return
+	}
 
 	h := &DishHeap{}
 	heap.Init(h)
 
-	for i, r := range ratings {
-		heap.Push(h, Dish{rating: r, index: i + 1})
+	for i, rating := range ratings {
+		heap.Push(h, Dish{rating: rating, index: i + 1})
 	}
 
-	var result int
-	for i := 0; i < k; i++ {
-		d := heap.Pop(h).(Dish)
-		result = d.index
+	for i := 0; i < dishCount-1; i++ {
+		heap.Pop(h)
 	}
 
-	fmt.Println(result)
+	dish := heap.Pop(h).(Dish)
+	fmt.Println(dish.index)
 }
