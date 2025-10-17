f271d97b09401e1a33cd40fc1a104bc8ef24ce22
Nekich06 <p.nikita.73526@gmail.com>
2025-10-17 09:19:32 +0300

    [TASK-2-1] Changed errors logic

--- nikita.petrov/task-2-1/cmd/service/main.go
+++ nikita.petrov/task-2-1/cmd/service/main.go
@@ -16,7 +16,7 @@ func main() {
 
 	_, err := fmt.Scan(&deptNum)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("cannot scan department number", err)
 
 		return
 	}
@@ -29,7 +29,7 @@ func main() {
 
 		_, err := fmt.Scan(&staffNum)
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("cannot scan staff number", err)
 
 			return
 		}
@@ -39,19 +39,25 @@ func main() {
 		for range staffNum {
 			_, err = fmt.Scan(&condition)
 			if err != nil {
-				fmt.Println(err)
+				fmt.Println("cannot scan condition", err)
 
 				return
 			}
 
 			_, err = fmt.Scan(&wishfulTemp)
 			if err != nil {
-				fmt.Println(err)
+				fmt.Println("cannot scan wishful temperature", err)
 
 				return
 			}
 
-			fmt.Println(airConditioner.SetAndGetNewOptimalTemp(condition, wishfulTemp))
+			newOptimalTemp, err := airConditioner.SetAndGetNewOptimalTemp(condition, wishfulTemp)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+			fmt.Println(newOptimalTemp)
 		}
 	}
 }
