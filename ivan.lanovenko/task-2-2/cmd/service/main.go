4508cc00ee769841c1847ea0062e17871575b5d3
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-10-18 22:17:42 +0300

    [TASK-2-2] fixed Pop method if n == 0, added panic in push, added erros in output

--- ivan.lanovenko/task-2-2/cmd/service/main.go
+++ ivan.lanovenko/task-2-2/cmd/service/main.go
@@ -7,14 +7,22 @@ import (
 
 type IntHeap []int
 
-func (h *IntHeap) Len() int           { return len(*h) }
-func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
-func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+func (h *IntHeap) Len() int {
+	return len(*h)
+}
+
+func (h *IntHeap) Less(i, j int) bool {
+	return (*h)[i] > (*h)[j]
+}
+
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
 
 func (h *IntHeap) Push(value any) {
 	num, ok := value.(int)
 	if !ok {
-		return
+		panic(fmt.Sprintf("IntHeap.Push: expected int, got %T", value))
 	}
 
 	*h = append(*h, num)
@@ -23,32 +31,21 @@ func (h *IntHeap) Push(value any) {
 func (h *IntHeap) Pop() any {
 	old := *h
 	n := len(old)
+	if n == 0 {
+		return nil
+	}
+
 	x := old[n-1]
 	*h = old[0 : n-1]
 
 	return x
 }
 
-func printRating(h IntHeap, sequenceNumber int) {
-	var value int
-
-	for range sequenceNumber {
-		num, ok := heap.Pop(&h).(int)
-		if !ok {
-			return
-		}
-
-		value = num
-	}
-
-	fmt.Println(value)
-}
-
 func main() {
 	var numberOfDishes int
 
 	if _, err := fmt.Scan(&numberOfDishes); err != nil {
-		fmt.Println("[WRONG INPUT]")
+		fmt.Println("Failed to read number of dishes: ", err)
 
 		return
 	}
@@ -60,7 +57,7 @@ func main() {
 		var current int
 
 		if _, err := fmt.Scan(&current); err != nil {
-			fmt.Println("[WRONG INPUT]")
+			fmt.Println("Failed to read current rating: ", err)
 
 			return
 		}
@@ -72,16 +69,27 @@ func main() {
 
 	_, err := fmt.Scan(&sequenceNumber)
 	if err != nil {
-		fmt.Println("[WRONG INPUT]")
+		fmt.Println("Failed to read sequenceNumber: ", err)
 
 		return
 	}
 
 	if sequenceNumber > numberOfDishes {
-		fmt.Println("[THE PRIORITY SEQUENCE NUMBER SHOULD NOT BE MORE THAN THE NUMBER OF DISHES]")
+		fmt.Println("The priority sequence number should not be more than the number of dishes")
 
 		return
 	}
 
-	printRating(*heapOfRatings, sequenceNumber)
+	var value int
+
+	for range sequenceNumber {
+		num, ok := heap.Pop(heapOfRatings).(int)
+		if !ok {
+			return
+		}
+
+		value = num
+	}
+
+	fmt.Println(value)
 }
