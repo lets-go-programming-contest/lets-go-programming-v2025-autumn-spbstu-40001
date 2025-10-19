6f4d0f159f0ddcc27936646a4950258127d82bcd
Nekich06 <p.nikita.73526@gmail.com>
2025-10-19 22:50:26 +0300

    [TASK-3] Implemented making directory and file

--- nikita.petrov/task-3/cmd/service/main.go
+++ nikita.petrov/task-3/cmd/service/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"flag"
-	"fmt"
 	"io"
 	"os"
+	"path"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -12,6 +14,16 @@ import (
 type Config struct {
 	InputFile  string `yaml:"input-file"`
 	OutputFile string `yaml:"output-file"`
+}
+
+type OutputFileInfo struct {
+	Dir      string
+	Filename string
+}
+
+func New(outputFile string) OutputFileInfo {
+	outputFilePath := strings.Split(outputFile, "/")
+	return OutputFileInfo{outputFilePath[0], outputFilePath[1]}
 }
 
 var configPathFlag = flag.String("config", "config.yaml", "path to config file")
@@ -32,9 +44,20 @@ func main() {
 	err = yaml.Unmarshal([]byte(configData), &files)
 
 	if err != nil {
-		panic("cannot unmarshal data")
+		panic("cannot unmarshal config data")
 	}
 
-	fmt.Println(files.InputFile)
-	fmt.Println(files.OutputFile)
+	outputFileInfo := New(files.OutputFile)
+
+	_, err = os.Stat(files.OutputFile)
+	if errors.Is(err, os.ErrNotExist) {
+		errMkDir := os.Mkdir(outputFileInfo.Dir, 0777)
+		if errMkDir != nil {
+			panic("cannot make directory")
+		}
+		_, errMkFile := os.OpenFile(path.Join(outputFileInfo.Dir, outputFileInfo.Filename), os.O_APPEND|os.O_CREATE|os.O_RDWR, 0777)
+		if errMkFile != nil {
+			panic("cannot create file")
+		}
+	}
 }
