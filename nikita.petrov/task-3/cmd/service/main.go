4c7e26de1e52db3fd2162059e78f358ceb5a5126
Nekich06 <p.nikita.73526@gmail.com>
2025-10-19 23:52:23 +0300

    [TASK-3] Implemented decoding valute course from xml to struct

--- nikita.petrov/task-3/cmd/service/main.go
+++ nikita.petrov/task-3/cmd/service/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/xml"
 	"errors"
 	"flag"
+	"fmt"
 	"io"
 	"os"
 	"path"
@@ -24,6 +26,20 @@ type OutputFileInfo struct {
 func New(outputFile string) OutputFileInfo {
 	outputFilePath := strings.Split(outputFile, "/")
 	return OutputFileInfo{outputFilePath[0], outputFilePath[1]}
+}
+
+type ValCurs struct {
+	XMLName xml.Name `xml:"ValCurs"`
+	Text    xml.CharData
+	Valute  []struct {
+		Text      xml.CharData
+		NumCode   string `xml:"NumCode"`
+		CharCode  string `xml:"CharCode"`
+		Nominal   string `xml:"Nominal"`
+		Name      string `xml:"Name"`
+		Value     string `xml:"Value"`
+		VunitRate string `xml:"VunitRate"`
+	}
 }
 
 var configPathFlag = flag.String("config", "config.yaml", "path to config file")
@@ -60,4 +76,18 @@ func main() {
 			panic("cannot create file")
 		}
 	}
+
+	inputFile, err := os.Open(files.InputFile)
+	if err != nil && os.IsNotExist(err) {
+		panic("input file does not exist")
+	}
+
+	decoder := xml.NewDecoder(inputFile)
+
+	var CentroBankValuteCourses ValCurs
+
+	if err := decoder.Decode(&CentroBankValuteCourses); err != nil {
+		panic(err)
+	}
+	fmt.Println(CentroBankValuteCourses)
 }
