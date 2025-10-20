800a418c3d1911cc358950e84b5f00236a1463b5
Danil3352 <danil.tkach2018@yandex.ru>
2025-10-20 19:17:37 +0300

    [TASK-3] Add processor.go (read xml + sort + write to json func)

---
+++ danil.tkach/task-3/internal/currency/processor.go
@@ -0,0 +1,127 @@
+package currency
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+const (
+	DirPerms  os.FileMode = 0o755
+	FilePerms os.FileMode = 0o644
+)
+
+type ValCurs struct {
+	XMLName xml.Name `xml:"ValCurs"`
+	Valutes []Valute `xml:"Valute"`
+}
+type Valute struct {
+	NumCode  int    `xml:"NumCode"`
+	CharCode string `xml:"CharCode"`
+	Nominal  int    `xml:"Nominal"`
+	Value    string `xml:"Value"`
+}
+
+type Currency struct {
+	NumCode  int
+	CharCode string
+	Value    float64
+}
+type ByValue []Currency
+
+func (a ByValue) Len() int           { return len(a) }
+func (a ByValue) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByValue) Less(i, j int) bool { return a[i].Value > a[j].Value }
+
+type CurrencyOutput struct {
+	NumCode  int     `json:"num_code"`
+	CharCode string  `json:"char_code"`
+	Value    float64 `json:"value"`
+}
+
+func readValCurs(inputFile string) (*ValCurs, error) {
+	xmlFile, err := os.ReadFile(inputFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read XML-file %s: %w", inputFile, err)
+	}
+
+	var valCurs ValCurs
+	if err := xml.Unmarshal(xmlFile, &valCurs); err != nil {
+		return nil, fmt.Errorf("failed to parse XML-file: %w", err)
+	}
+
+	fmt.Printf("Successful read %d currencies.\n", len(valCurs.Valutes))
+
+	return &valCurs, nil
+}
+
+func transformAndSort(valCurs *ValCurs) ([]Currency, error) {
+	currencies := make([]Currency, 0, len(valCurs.Valutes))
+
+	for _, valute := range valCurs.Valutes {
+		valueStr := strings.Replace(valute.Value, ",", ".", 1)
+
+		value, err := strconv.ParseFloat(valueStr, 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed convert '%s' to number: %w", valute.Value, err)
+		}
+
+		realValue := value / float64(valute.Nominal)
+
+		currencies = append(currencies, Currency{
+			NumCode:  valute.NumCode,
+			CharCode: valute.CharCode,
+			Value:    realValue,
+		})
+	}
+
+	sort.Sort(ByValue(currencies))
+
+	return currencies, nil
+}
+
+func writeResult(currencies []Currency, outputFile string) error {
+	outputDir := filepath.Dir(outputFile)
+	if err := os.MkdirAll(outputDir, DirPerms); err != nil {
+		return fmt.Errorf("failed to create a dir %s: %w", outputDir, err)
+	}
+
+	outputData := make([]CurrencyOutput, 0, len(currencies))
+	for _, c := range currencies {
+		outputData = append(outputData, CurrencyOutput(c))
+	}
+
+	jsonData, err := json.MarshalIndent(outputData, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to read to json: %w", err)
+	}
+
+	if err := os.WriteFile(outputFile, jsonData, FilePerms); err != nil {
+		return fmt.Errorf("failed write file %s: %w", outputFile, err)
+	}
+
+	return nil
+}
+
+func Process(inputFile, outputFile string) error {
+	valCurs, err := readValCurs(inputFile)
+	if err != nil {
+		return err
+	}
+
+	currencies, err := transformAndSort(valCurs)
+	if err != nil {
+		return err
+	}
+
+	if err := writeResult(currencies, outputFile); err != nil {
+		return err
+	}
+
+	return nil
+}
