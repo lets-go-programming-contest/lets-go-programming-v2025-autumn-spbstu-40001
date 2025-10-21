314e8dca38c221802981bc751ae795f8a1271d90
Nekich06 <p.nikita.73526@gmail.com>
2025-10-22 00:21:23 +0300

    [TASK-3] Ooops, forgot flag parsing

--- nikita.petrov/task-3/cmd/service/main.go
+++ nikita.petrov/task-3/cmd/service/main.go
@@ -19,11 +19,14 @@ import (
 func main() {
 	var configPathFlag = flag.String("config", "config.yaml", "path to config file")
 
+	flag.Parse()
+
 	configFile := fmanager.GetConfigFile(configPathFlag)
 	configData := fmanager.GetConfigData(configFile)
 
 	var files fmanager.Config
 	err := yaml.Unmarshal(configData, &files)
+
 	if err != nil {
 		panic(err)
 	}
@@ -43,12 +46,12 @@ func main() {
 
 	XMLDecoder := xml.NewDecoder(inputFile)
 
-	var CB currencyrate.CurrencyRate
-	if err := XMLDecoder.Decode(&CB); err != nil {
+	var CBCurrencyRate currencyrate.CurrencyRate
+	if err := XMLDecoder.Decode(&CBCurrencyRate); err != nil {
 		panic(err)
 	}
 
-	sort.Sort(valutessorter.ByValue(CB))
+	sort.Sort(valutessorter.ByValue(CBCurrencyRate))
 
 	outputFile, err := os.OpenFile(path.Join(dir, filename), os.O_WRONLY, 0777)
 	if err != nil {
@@ -57,7 +60,8 @@ func main() {
 
 	JSONEncoder := json.NewEncoder(outputFile)
 	JSONEncoder.SetIndent("", "\t")
-	err = JSONEncoder.Encode(&CB.Valute)
+	err = JSONEncoder.Encode(&CBCurrencyRate.Valute)
+
 	if err != nil {
 		panic(err)
 	}
