aed53409f08cb7cc20877b6a1679ec15b8c8b04e
Angelina Vladimirova <verticalochka@gmail.com>
2025-10-21 22:37:58 +0300

    [TASK-2-2] fix lint

--- angelina.vladimirova/task-2-2/cmd/service/main.go
+++ angelina.vladimirova/task-2-2/cmd/service/main.go
@@ -26,6 +26,7 @@ func (h *MaxHeap) Less(i, j int) bool {
 	if i >= len(h.data) || j >= len(h.data) || i < 0 || j < 0 {
 		return false
 	}
+
 	return h.data[i] > h.data[j]
 }
 
@@ -33,6 +34,7 @@ func (h *MaxHeap) Swap(i, j int) {
 	if i >= len(h.data) || j >= len(h.data) || i < 0 || j < 0 {
 		return
 	}
+
 	h.data[i], h.data[j] = h.data[j], h.data[i]
 }
 
@@ -41,48 +43,59 @@ func (h *MaxHeap) Push(x interface{}) {
 	if !ok {
 		return
 	}
+
 	h.data = append(h.data, value)
 }
 
 func (h *MaxHeap) Pop() interface{} {
 	length := len(h.data)
+
 	if length == 0 {
 		return -1
 	}
+
 	x := h.data[length-1]
 	h.data = h.data[0 : length-1]
+
 	return x
 }
 
 func readCount() (int, error) {
 	var count int
+
 	_, err := fmt.Scan(&count)
 	if err != nil {
 		return 0, fmt.Errorf("read count: %w", err)
 	}
+
 	if count <= 0 {
 		return 0, fmt.Errorf("%w: %d", ErrInvalidCount, count)
 	}
+
 	return count, nil
 }
 
 func readRatings(count int) ([]int, error) {
 	ratings := make([]int, count)
+
 	for index := range ratings {
 		_, err := fmt.Scan(&ratings[index])
 		if err != nil {
 			return nil, fmt.Errorf("read rating: %w", err)
 		}
 	}
+
 	return ratings, nil
 }
 
 func readPositionK() (int, error) {
 	var positionK int
+
 	_, err := fmt.Scan(&positionK)
 	if err != nil {
 		return 0, fmt.Errorf("read k: %w", err)
 	}
+
 	return positionK, nil
 }
 
@@ -118,10 +131,17 @@ func findKthLargest(ratings []int, positionK int) (int, error) {
 
 	for range positionK {
 		value := heap.Pop(maxHeap)
+
 		if value == -1 {
 			return -1, ErrHeapEmpty
 		}
-		result = value.(int)
+
+		intValue, ok := value.(int)
+		if !ok {
+			return -1, ErrUnexpectedType
+		}
+
+		result = intValue
 		found = true
 	}
 
@@ -136,22 +156,26 @@ func main() {
 	count, ratings, positionK, err := readInput()
 	if err != nil {
 		fmt.Printf("Invalid input: %v\n", err)
+
 		return
 	}
 
 	if positionK < 1 || positionK > count {
 		fmt.Println("There is no such dish")
+
 		return
 	}
 
 	result, err := findKthLargest(ratings, positionK)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
+
 		return
 	}
 
 	if result == -1 {
 		fmt.Println("There is no such dish")
+
 		return
 	}
 
