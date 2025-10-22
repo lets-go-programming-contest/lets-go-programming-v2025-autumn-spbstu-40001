bb1d4394fceeff210464fc71da9b4b87da95d60c
Nekich06 <p.nikita.73526@gmail.com>
2025-10-22 17:24:09 +0300

    [TASK-3] Changed type of NumCode field to int

--- nikita.petrov/task-3/internal/currencyrate/currencyrate.go
+++ nikita.petrov/task-3/internal/currencyrate/currencyrate.go
@@ -2,7 +2,7 @@ package currencyrate
 
 type CurrencyRate struct {
 	Valute []struct {
-		NumCode  string `json:"num_code"  xml:"NumCode"`
+		NumCode  int    `json:"num_code"  xml:"NumCode"`
 		CharCode string `json:"char_code" xml:"CharCode"`
 		Value    string `json:"value"     xml:"Value"`
 	}
