361d067485d559bc046c0ff1aa85bcdd4e293dd9
Nekich06 <p.nikita.73526@gmail.com>
2025-10-24 01:45:19 +0300

    [TASK-3] YaYaFix linter

--- nikita.petrov/task-3/internal/currencyrate/currencyrate.go
+++ nikita.petrov/task-3/internal/currencyrate/currencyrate.go
@@ -10,9 +10,9 @@ type floatWithDots float64
 
 type CurrencyRate struct {
 	Valute []struct {
-		NumCode  int           `json:"num_code"  xml:"NumCode"`
-		CharCode string        `json:"char_code" xml:"CharCode"`
-		Value    floatWithDots `json:"value"     xml:"Value"`
+		NumCode  int           `xml:"NumCode"  json:"num_code"`
+		CharCode string        `xml:"CharCode" json:"char_code"`
+		Value    floatWithDots `xml:"Value"    json:"value"`
 	}
 }
 
