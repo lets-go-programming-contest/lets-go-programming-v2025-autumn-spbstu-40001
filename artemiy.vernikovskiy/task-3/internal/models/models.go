ac77f4b3cc24caa1f594e6862778abcf7f50f309
Aapng-cmd <verart1@yandex.ru>
2025-10-23 16:20:06 -0500

    [TASK-3] Let this result live, please, have mercy on me

--- artemiy.vernikovskiy/task-3/internal/models/models.go
+++ artemiy.vernikovskiy/task-3/internal/models/models.go
@@ -1,6 +1,38 @@
 package models
 
+import (
+	"encoding/xml"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type CommaFloat float64 // please, let this live
+
+func (cf *CommaFloat) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	// let it be here, please, i do not want one more packet in this small task
+	var sIsAWorkingStringForFloatsWithCommaIsThisNameLongEnough string
+
+	err := d.DecodeElement(&sIsAWorkingStringForFloatsWithCommaIsThisNameLongEnough, &start)
+	if err != nil {
+		return fmt.Errorf("ah, kozache, UnmarshalXML override func failed: %w", err)
+	}
+
+	sIsAWorkingStringForFloatsWithCommaIsThisNameLongEnough = strings.ReplaceAll(
+		sIsAWorkingStringForFloatsWithCommaIsThisNameLongEnough,
+		",",
+		".",
+	)
+
+	val, err := strconv.ParseFloat(sIsAWorkingStringForFloatsWithCommaIsThisNameLongEnough, 64)
+	if err != nil {
+		return fmt.Errorf("ah, kozache, UnmarshalXML override func failed: %w", err)
+	}
+
+	*cf = CommaFloat(val)
+
+	return nil
+}
 
 type Settings struct {
 	InputFileSetting  string `yaml:"input-file"`
