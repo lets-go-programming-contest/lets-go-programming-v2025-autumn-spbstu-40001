883d331dc77ae7cca7fa042e71fb6a7c50be1f97
“Timofeev <maksimtmfv@gmail.com>
2025-10-23 14:47:36 +0300

    [TASK-3] Initial commit

---
+++ maxim.timofeev/task-3/cmd/service/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"sort"
+
+	"github.com/PigoDog/task-3/internal/IOcurrency"
+	"github.com/PigoDog/task-3/internal/config"
+)
+
+func main() {
+	configPath := flag.String("config", "", "Path to YAML config")
+	flag.Parse()
+
+	if *configPath == "" {
+		panic("flag --config is empty")
+	}
+
+	config, err := config.ReadConfig(*configPath)
+
+	if err != nil {
+		panic(fmt.Errorf("failed to read config: %v", err))
+	}
+
+	valutes, err := IOcurrency.LoadXML(config.InputFile)
+
+	if err != nil {
+		panic(fmt.Errorf("failed to read XML: %v", err))
+	}
+
+	sort.Slice(valutes, func(i, j int) bool {
+		return valutes[i].Value > valutes[j].Value
+	})
+
+	err = IOcurrency.SaveJSON(config.OutputFile, valutes)
+
+	if err != nil {
+		panic(fmt.Errorf("failed to read JSON: %v", err))
+	}
+}
