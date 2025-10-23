f2f45d16f891ecf80fbde868bdbd77b096a162e5
Nekich06 <p.nikita.73526@gmail.com>
2025-10-24 01:00:32 +0300

    [TASK-3] Implemented parsing float64 while unmarshaling XML file

--- nikita.petrov/task-3/internal/currencyrate/currencyrate.go
+++ nikita.petrov/task-3/internal/currencyrate/currencyrate.go
@@ -1,9 +1,37 @@
 package currencyrate
 
+import (
+	"encoding/xml"
+	"strconv"
+	"strings"
+)
+
+type floatWithDots float64
+
 type CurrencyRate struct {
-	Valute []struct {
-		NumCode  string `json:"num_code"  xml:"NumCode"`
-		CharCode string `json:"char_code" xml:"CharCode"`
-		Value    string `json:"value"     xml:"Value"`
+	Valute []*singleValute
+}
+
+type singleValute struct {
+	NumCode  int           `json:"num_code"  xml:"NumCode"`
+	CharCode string        `json:"char_code" xml:"CharCode"`
+	Value    floatWithDots `json:"value"     xml:"Value"`
+}
+
+func (fd *floatWithDots) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var content string
+	err := d.DecodeElement(&content, &start)
+	if err != nil {
+
+		return err
 	}
+	content = strings.Replace(content, ",", ".", -1)
+	var retFloat64 float64
+	retFloat64, err = strconv.ParseFloat(content, 64)
+	if err != nil {
+		panic(err)
+	}
+	*fd = floatWithDots(retFloat64)
+
+	return nil
 }
