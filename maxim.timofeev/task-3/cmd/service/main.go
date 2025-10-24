79ee9d3be8baf2cb30270f2dcc7dbf33c529dfef
“Timofeev <maksimtmfv@gmail.com>
2025-10-24 13:54:37 +0300

    [TASK-3] linter

--- maxim.timofeev/task-3/cmd/service/main.go
+++ maxim.timofeev/task-3/cmd/service/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"sort"
 
 	"github.com/PigoDog/task-3/internal/IOcurrency"
 	"github.com/PigoDog/task-3/internal/config"
@@ -20,20 +18,13 @@ func main() {
 	config, err := config.ReadConfig(*configPath)
 
 	if err != nil {
-		panic(fmt.Errorf("failed to read config: %v", err))
+		panic(err.Error())
 	}
 
-	valutes, err := IOcurrency.LoadXML(config.InputFile)
+	var valutes IOcurrency.ValCurs
+	valutes.ReadXML(config.InputFile)
 
-	if err != nil {
-		panic(fmt.Errorf("failed to read XML: %v", err))
-	}
-
-	sort.Slice(valutes, func(i, j int) bool {
-		return valutes[i].Value > valutes[j].Value
-	})
-
-	if err = IOcurrency.SaveJSON(config.OutputFile, valutes); err != nil {
-		panic(fmt.Errorf("failed to read JSON: %v", err))
+	if err = IOcurrency.SaveJSON(config.OutputFile, valutes.Valutes); err != nil {
+		panic(err.Error())
 	}
 }
