caa843021fa753bd01104f40e0d4215ed3052599
Danil3352 <danil.tkach2018@yandex.ru>
2025-10-28 19:54:55 +0300

    [TASK-3] Fix comment (add models + did func universal)

--- danil.tkach/task-3/internal/xml/xmlparser.go
+++ danil.tkach/task-3/internal/xml/xmlparser.go
@@ -6,24 +6,12 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strconv"
-	"strings"
 
+	"github.com/Danil3352/task-3/internal/models"
 	"golang.org/x/net/html/charset"
 )
 
-type ValCurs struct {
-	Valutes []Currency `xml:"Valute"`
-}
-
-type Currency struct {
-	NumCode    int     `json:"num_code"  xml:"NumCode"`
-	CharCode   string  `json:"char_code" xml:"CharCode"`
-	ValueField string  `json:"-"         xml:"Value"`
-	Value      float64 `json:"value"     xml:"-"`
-}
-
-func ReadValCurs(inputFile string) (*ValCurs, error) {
+func ReadValCurs(inputFile string) (*models.ValCurs, error) {
 	xmlData, err := os.ReadFile(inputFile)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read XML-file %s: %w", inputFile, err)
@@ -35,21 +23,9 @@ func ReadValCurs(inputFile string) (*ValCurs, error) {
 		return charset.NewReader(input, c)
 	}
 
-	var valCurs ValCurs
+	var valCurs models.ValCurs
 	if err := decoder.Decode(&valCurs); err != nil {
 		return nil, fmt.Errorf("failed to parse XML-file: %w", err)
-	}
-
-	for i := range valCurs.Valutes {
-		valute := &valCurs.Valutes[i]
-		valueStr := strings.Replace(valute.ValueField, ",", ".", 1)
-
-		value, err := strconv.ParseFloat(valueStr, 64)
-		if err != nil {
-			return nil, fmt.Errorf("failed convert '%s' to number: %w", valute.ValueField, err)
-		}
-
-		valute.Value = value
 	}
 
 	return &valCurs, nil
