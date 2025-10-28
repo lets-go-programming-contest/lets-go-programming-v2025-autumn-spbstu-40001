4f57c09923bbdb39adc17f79a504c716733fa829
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-10-28 14:31:40 +0300

    [TASK-3] add json writer

--- aleksey.maslov/task-3/internal/jsonwriter/jsonwriter.go
+++ aleksey.maslov/task-3/internal/jsonwriter/jsonwriter.go
@@ -1,9 +1,27 @@
 package jsonwriter
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+
 	"github.com/A1exMas1ov/task-3/internal/currency"
 )
 
 func SaveJSON(path string, valutes []currency.Valute) error {
+
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "\t")
+
+	if err := encoder.Encode(valutes); err != nil {
+		return fmt.Errorf("failed to encode JSON: %w", err)
+	}
+
 	return nil
+
 }
