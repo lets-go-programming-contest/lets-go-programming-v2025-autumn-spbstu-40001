7b951e13ed3d166bbba726d21e06b42412dbb4ac
Angelina Vladimirova <verticalochka@gmail.com>
2025-10-28 21:43:32 +0300

    [TASK-3] fix lint2

--- angelina.vladimirova/task-3/cmd/service/main.go
+++ angelina.vladimirova/task-3/cmd/service/main.go
@@ -1,35 +1,105 @@
 package main
 
 import (
+	"encoding/json"
+	"encoding/xml"
 	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
 	"sort"
-	"task-3/internal/config"
-	"task-3/internal/currency"
-	"task-3/internal/json"
-	"task-3/internal/xml"
+	"strings"
+
+	"golang.org/x/net/html/charset"
+	"gopkg.in/yaml.v3"
 )
+
+type Config struct {
+	InputFile  string `yaml:"input-file"`
+	OutputFile string `yaml:"output-file"`
+}
+
+type Currency struct {
+	NumCode  int     `json:"num_code" xml:"NumCode"`
+	CharCode string  `json:"char_code" xml:"CharCode"`
+	Value    float64 `json:"value" xml:"Value"`
+}
+
+type ValCurs struct {
+	Valutes []Currency `xml:"Valute"`
+}
 
 func main() {
 	configPath := flag.String("config", "config.yaml", "path to config")
 	flag.Parse()
 
-	config, err := config.ParseYaml(*configPath)
+	config, err := readConfig(*configPath)
 	if err != nil {
 		panic(err)
 	}
 
-	currencyList := currency.Rates{Data: []currency.Currency{}}
-	err = xml.ParseXML(config.InputFilePath, &currencyList)
+	valCurs, err := readXML(config.InputFile)
 	if err != nil {
 		panic(err)
 	}
 
-	sort.Slice(currencyList.Data, func(i, j int) bool {
-		return currencyList.Data[i].Value > currencyList.Data[j].Value
+	sort.Slice(valCurs.Valutes, func(i, j int) bool {
+		return valCurs.Valutes[i].Value > valCurs.Valutes[j].Value
 	})
 
-	err = json.ParseJSON(config.OutputFilePath, currencyList.Data)
+	err = writeJSON(valCurs.Valutes, config.OutputFile)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func readConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read config file: %w", err)
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, fmt.Errorf("cannot unmarshal YAML: %w", err)
+	}
+
+	return &config, nil
+}
+
+func readXML(path string) (*ValCurs, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read XML file: %w", err)
+	}
+
+	var valCurs ValCurs
+	decoder := xml.NewDecoder(strings.NewReader(string(data)))
+	decoder.CharsetReader = charset.NewReaderLabel
+
+	err = decoder.Decode(&valCurs)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse XML: %w", err)
+	}
+
+	return &valCurs, nil
+}
+
+func writeJSON(data []Currency, path string) error {
+	jsonData, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return fmt.Errorf("cannot marshal JSON: %w", err)
+	}
+
+	dir := filepath.Dir(path)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("cannot create directory: %w", err)
+	}
+
+	if err := os.WriteFile(path, jsonData, 0644); err != nil {
+		return fmt.Errorf("cannot write file: %w", err)
+	}
+
+	return nil
 }
