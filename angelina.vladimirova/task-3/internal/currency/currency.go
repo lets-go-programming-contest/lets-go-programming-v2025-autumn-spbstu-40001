d3cd4780ea5f347126a968a150fbaeaec230de74
Angelina Vladimirova <verticalochka@gmail.com>
2025-10-28 23:13:37 +0300

    [TASK-3] fixx lint

--- angelina.vladimirova/task-3/internal/currency/currency.go
+++ angelina.vladimirova/task-3/internal/currency/currency.go
@@ -21,17 +21,20 @@ type FloatforCur float64
 
 func (cf *FloatforCur) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) error {
 	var valueStr string
+
 	err := decoder.DecodeElement(&valueStr, &start)
 	if err != nil {
 		return fmt.Errorf("failed to parse value: %w", err)
 	}
 
 	normalized := strings.Replace(valueStr, ",", ".", 1)
+
 	value, err := strconv.ParseFloat(normalized, 64)
 	if err != nil {
 		return fmt.Errorf("invalid number %q: %w", valueStr, err)
 	}
 
 	*cf = FloatforCur(value)
+
 	return nil
 }
