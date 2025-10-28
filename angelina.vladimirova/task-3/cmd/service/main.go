fcaa50cf41a6f3ba4eb86319823a75b2fe29ca41
Angelina Vladimirova <verticalochka@gmail.com>
2025-10-28 22:50:13 +0300

    [TASK-3] fix

--- angelina.vladimirova/task-3/cmd/service/main.go
+++ angelina.vladimirova/task-3/cmd/service/main.go
@@ -1,4 +1,3 @@
-// meow
 package main
 
 import (
--- angelina.vladimirova/task-3/internal/xml/parsexml.go
+++ angelina.vladimirova/task-3/internal/xml/parsexml.go
@@ -1,16 +1,16 @@
-package json
+package xml
 
 import (
-	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"os"
 	"path/filepath"
 )
 
-func ParseJSON(filePath string, data interface{}) error {
-	jsonData, err := json.MarshalIndent(data, "", "\t")
+func ParseXML(filePath string, data interface{}) error {
+	xmlData, err := xml.MarshalIndent(data, "", "\t")
 	if err != nil {
-		return fmt.Errorf("serialize to JSON: %w", err)
+		return fmt.Errorf("serialize to XML: %w", err)
 	}
 
 	directory := filepath.Dir(filePath)
@@ -18,7 +18,7 @@ func ParseJSON(filePath string, data interface{}) error {
 		return fmt.Errorf("cannot create directory '%s': %w", directory, err)
 	}
 
-	if err := os.WriteFile(filePath, jsonData, 0o600); err != nil {
+	if err := os.WriteFile(filePath, xmlData, 0o600); err != nil {
 		return fmt.Errorf("cannot write to file '%s': %w", filePath, err)
 	}
 
