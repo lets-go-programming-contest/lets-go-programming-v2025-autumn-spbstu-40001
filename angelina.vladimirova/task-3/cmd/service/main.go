362748689c1310eb721a4745a80508f7ad8f7073
Angelina Vladimirova <verticalochka@gmail.com>
2025-10-28 20:51:33 +0300

    [TASK-3] initial commit

---
+++ angelina.vladimirova/task-3/cmd/service/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"sort"
+
+	"github.com/angelina.vladimirova/task-3/internal/config"
+	"github.com/angelina.vladimirova/task-3/internal/currency"
+	"github.com/angelina.vladimirova/task-3/internal/json"
+	"github.com/angelina.vladimirova/task-3/internal/xml"
+)
+
+func main() {
+	configPath := flag.String("config", "config.yaml", "path to config")
+	flag.Parse()
+
+	config, err := config.ParseYaml(*configPath)
+	if err != nil {
+		panic(err)
+	}
+
+	currencyList := currency.Rates{Data: []currency.Currency{}}
+	err = xml.ParseXML(config.InputFilePath, &currencyList)
+	if err != nil {
+		panic(err)
+	}
+
+	sort.Slice(currencyList.Data, func(i, j int) bool {
+		return currencyList.Data[i].Value > currencyList.Data[j].Value
+	})
+
+	err = json.ParseJSON(config.OutputFilePath, currencyList.Data)
+	if err != nil {
+		panic(err)
+	}
+}
