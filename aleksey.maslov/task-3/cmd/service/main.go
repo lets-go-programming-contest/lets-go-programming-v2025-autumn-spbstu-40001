91ba3ff19c0bbdd1307d961ad65b199041f2529c
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-10-29 04:55:38 +0300

    [TASK-3] changed the names

--- aleksey.maslov/task-3/cmd/service/main.go
+++ aleksey.maslov/task-3/cmd/service/main.go
@@ -4,13 +4,13 @@ import (
 	"flag"
 
 	"github.com/A1exMas1ov/task-3/internal/config"
-	"github.com/A1exMas1ov/task-3/internal/currency"
 	"github.com/A1exMas1ov/task-3/internal/jsonwriter"
+	"github.com/A1exMas1ov/task-3/internal/models"
 	"github.com/A1exMas1ov/task-3/internal/xmlparser"
 )
 
 func main() {
-	configPath := flag.String("config", "configs/config.yaml", "path to config file")
+	configPath := flag.String("config", "config/config.yaml", "path to config file")
 	flag.Parse()
 
 	config, err := config.ReadConfig(*configPath)
@@ -18,7 +18,7 @@ func main() {
 		panic(err)
 	}
 
-	var valCurs currency.ValCurs
+	var valCurs models.ValCurs
 
 	err = xmlparser.ParseXML(config.InputFile, &valCurs)
 	if err != nil {
