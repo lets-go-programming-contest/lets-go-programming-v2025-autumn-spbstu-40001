057675c9eeb95f29b2d9d326bd97a21d0ab00dd7
atroxxx <danilandriuschin@yandex.ru>
2025-10-30 19:14:04 +0300

    [TASK-3] add json & xml parsers

---
+++ danil.andryushin/task-3/internal/parsing/xml/xml.go
@@ -0,0 +1,26 @@
+package xml
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html/charset"
+)
+
+func Unmarshall(path string, obj any) error {
+	input, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("failed to read file: %w", err)
+	}
+
+	decoder := xml.NewDecoder(bytes.NewReader(input))
+	decoder.CharsetReader = charset.NewReaderLabel
+	err = decoder.Decode(&obj)
+	if err != nil {
+		return fmt.Errorf("unmarshalling failed: %w", err)
+	}
+
+	return nil
+}
