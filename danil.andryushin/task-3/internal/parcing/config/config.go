dbbbf8a4f1b8e5edd18a428be55c5be847ec3312
atroxxx <danilandriuschin@yandex.ru>
2025-10-30 18:25:58 +0300

    [TASK-3] add valute struct

--- danil.andryushin/task-3/internal/parcing/config/config.go
+++ danil.andryushin/task-3/internal/parcing/config/config.go
@@ -1,31 +1,26 @@
 package config
 
 import (
-	"errors"
+	"fmt"
 	"os"
 
 	"gopkg.in/yaml.v3"
 )
 
-var ErrInvalidConfig = errors.New("invalid config format (expected .yaml)")
-
-func New(path string) (Config, error) {
-	var config = Config{}
-	if len(path) < 5 || path[len(path)-5:] != ".yaml" {
-		return config, ErrInvalidConfig
-	}
-	data, err := os.ReadFile(path)
-	if err != nil {
-		return config, err
-	}
-	err = yaml.Unmarshal(data, &config)
-	if err != nil {
-		return config, err
-	}
-	return config, nil
-}
-
 type Config struct {
 	InputFile  string `yaml:"input-file"`
 	OutputFile string `yaml:"output-file"`
+}
+
+func New(path string) (Config, error) {
+	var config Config
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return config, fmt.Errorf("cannot read config file: %w", err)
+	}
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return config, fmt.Errorf("unarshallig failed: %w", err)
+	}
+	return config, nil
 }
