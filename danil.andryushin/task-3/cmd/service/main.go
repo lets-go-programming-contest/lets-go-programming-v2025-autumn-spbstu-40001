86497d429dff4c2ca2237c36026163827f30ee6b
atroxxx <danilandriuschin@yandex.ru>
2025-10-30 19:14:57 +0300

    [TASK-3] main logic implementation

--- danil.andryushin/task-3/cmd/service/main.go
+++ danil.andryushin/task-3/cmd/service/main.go
@@ -3,18 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 
-	"github.com/atroxxxxxx/task-3/internal/parcing/config"
+	"github.com/atroxxxxxx/task-3/internal/parsing/json"
+	"github.com/atroxxxxxx/task-3/internal/parsing/xml"
+	"github.com/atroxxxxxx/task-3/internal/parsing/yaml"
+	"github.com/atroxxxxxx/task-3/internal/valute"
 )
+
+const Permission = 0o666
 
 func main() {
 	args := os.Args
 	if len(args) < 3 || args[1] != "-config" {
 		fmt.Println("invalid args")
 	}
-	data, err := config.New(args[2])
+	data, err := yaml.Unmarshall(args[2])
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(data)
+
+	var valuteSlice valute.ValuteSlice
+	err = xml.Unmarshall(data.InputFile, &valuteSlice)
+	if err != nil {
+		panic(err)
+	}
+	sort.Slice(valuteSlice.Valutes, func(i, j int) bool {
+		return valuteSlice.Valutes[i].Value > valuteSlice.Valutes[j].Value
+	})
+	err = json.WriteToFile(data.OutputFile, valuteSlice.Valutes, Permission)
+	if err != nil {
+		panic(err)
+	}
 }
