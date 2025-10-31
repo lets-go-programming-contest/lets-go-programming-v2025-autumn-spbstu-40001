eed4095b819fa19898999e8021d8fe53910821a8
guseyn <ganx.gamer@gmail.com>
2025-10-31 15:33:04 +0300

    [TASK-3] Add XML parser

---
+++ guseyn.guseynov/task-3/interanal/xmlparcer/xmlparcer.go
@@ -0,0 +1,21 @@
+package xmlparser
+
+import (
+	"bytes"
+	"encoding/xml"
+	"os"
+
+	"golang.org/x/net/html/charset"
+)
+
+func Parse(path string, result any) error {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	decoder := xml.NewDecoder(bytes.NewReader(data))
+	decoder.CharsetReader = charset.NewReaderLabel
+
+	return decoder.Decode(result)
+}
