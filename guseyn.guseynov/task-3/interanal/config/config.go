385fab431111049ec119d7dea6a0fff23e7a9151
guseyn <ganx.gamer@gmail.com>
2025-10-31 15:28:00 +0300

    [TASK-3] add config

---
+++ guseyn.guseynov/task-3/interanal/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	InputFile  string `yaml:"input-file"`
+	OutputFile string `yaml:"output-file"`
+}
+
+func Load(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
