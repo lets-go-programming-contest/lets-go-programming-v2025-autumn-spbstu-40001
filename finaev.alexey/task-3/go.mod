257ae7a3425ea9cd7a6eeba4db568ec77e01e1e9
AlexeyFinaev02 <finaev0212212@mail.ru>
2025-10-31 14:50:38 +0300

    [TASK-3] first vers lab task-3

--- finaev.alexey/task-3/go.mod
+++ finaev.alexey/task-3/go.mod
@@ -1,3 +1,7 @@
 module github.com/AlexeyFinaev02/task-3
 
 go 1.22.7
+require (
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ finaev.alexey/task-3/internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	InputFile  string `yaml:"input-file"`
+	OutputFile string `yaml:"output-file"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+    file, err := os.Open(configPath)
+    if err != nil {
+        return nil, fmt.Errorf("Failed open file: %w", err)
+    }
+    defer file.Close()
+
+    data, err := io.ReadAll(file)
+    if err != nil {
+        return nil, fmt.Errorf("Failed read file: %w", err)
+    }
+
+    var config Config
+
+    err = yaml.Unmarshal(data, &config)
+    if err != nil {
+        return nil, fmt.Errorf("Failed parse YAML: %w", err)
+    }
+
+    if config.InputFile == "" {
+        return nil, fmt.Errorf("input-file must be filled in")
+    }
+    if config.OutputFile == "" {
+        return nil, fmt.Errorf("output-file must be filled in")
+    }
+
+    return &config, nil
+}
