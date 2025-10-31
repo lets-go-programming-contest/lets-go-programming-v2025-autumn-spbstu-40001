1e74753d6dc8741923e9ad1a0c937ff86800f50f
AlexeyFinaev02 <finaev0212212@mail.ru>
2025-10-31 18:13:01 +0300

    [TASK-3] last fix lint

--- finaev.alexey/task-3/internal/jsonwriter/jsonwriter.go
+++ finaev.alexey/task-3/internal/jsonwriter/jsonwriter.go
@@ -25,6 +25,7 @@ func SaveJSONwithPerms(outputPath string, data any, dirPerm, filePerm os.FileMod
 	if err != nil {
 		return fmt.Errorf("marshal JSON: %w", err)
 	}
+
 	err = os.WriteFile(outputPath, jsonData, filePerm)
 	if err != nil {
 		return fmt.Errorf("write file: %w", err)
