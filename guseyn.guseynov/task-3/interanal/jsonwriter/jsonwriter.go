6957187f3c18b194bc0fe315f26e5eb06cc6c186
guseyn <ganx.gamer@gmail.com>
2025-10-31 15:35:27 +0300

    [TASK-3] Add jsonwriter

---
+++ guseyn.guseynov/task-3/interanal/jsonwriter/jsonwriter.go
@@ -0,0 +1,26 @@
+package jsonwriter
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+func Write(path string, data any) error {
+	dir := filepath.Dir(path)
+	err := os.MkdirAll(dir, 0755)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "\t")
+
+	return encoder.Encode(data)
+}
