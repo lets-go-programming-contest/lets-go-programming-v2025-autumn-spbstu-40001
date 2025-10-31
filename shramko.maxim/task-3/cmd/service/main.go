dfa8a7a787ba49dc5572ba2d067435d5bea431f2
Maksim_Shramko <maxramko@gmail.com>
2025-10-31 23:29:57 +0300

    [TASK-3] Progressing main

--- shramko.maxim/task-3/cmd/service/main.go
+++ shramko.maxim/task-3/cmd/service/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"flag"
 
+	"github.com/Elektrek/currency-processor/internal/currencyio"
+	"github.com/Elektrek/currency-processor/internal/settings"
+)
+
+func main() {
+	cfgPath := flag.String("config", "", "Configuration file path")
+	flag.Parse()
+
+	if *cfgPath == "" {
+		panic("Configuration file path is required")
+	}
+
+	config, err := settings.LoadConfig(*cfgPath)
+	if err != nil {
+		panic(err)
+	}
+
+	var rates currencyio.ExchangeRates
+	rates.LoadFromXML(config.Source)
+	rates.SortByRate()
+
+	if err := currencyio.WriteJSONFile(config.Result, rates.Currencies); err != nil {
+		panic(err)
+	}
 }
---
+++ shramko.maxim/task-3/go.mod
@@ -0,0 +1,8 @@
+module github.com/Elektrek/currency-processor
+
+go 1.22.7
+
+require (
+	golang.org/x/text v0.14.0
+	gopkg.in/yaml.v3 v3.0.1
+)
