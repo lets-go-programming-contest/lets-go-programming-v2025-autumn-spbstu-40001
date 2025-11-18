08f945c19011ea73a2b000d5c9cdb368574ef0e6
Vika <vikagluskova2006@gmail.com>
2025-11-18 21:55:14 +0300

    [TASK-3] fix c commentarii

---
+++ victoria.glushkova/task-3/internal/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	InputFile  string `yaml:"input-file"`
+	OutputFile string `yaml:"output-file"`
+}
+
+var ErrConfigFieldsRequired = errors.New("config file must contain both input-file and output-file fields")
+
+func ReadConfig(configPath string) (*Config, error) {
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open config file: %w", err)
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read config file: %w", err)
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, fmt.Errorf("cannot parse YAML config: %w", err)
+	}
+
+	if config.InputFile == "" || config.OutputFile == "" {
+		return nil, ErrConfigFieldsRequired
+	}
+
+	return &config, nil
+}
--- victoria.glushkova/task-3/internal/xml/parsexml.go
+++ victoria.glushkova/task-3/internal/xml/parsexml.go
@@ -6,67 +6,19 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strconv"
-	"strings"
 
 	"golang.org/x/net/html/charset"
 )
 
 type ValCurs struct {
 	XMLName xml.Name `xml:"ValCurs"`
-	Date    string   `xml:"Date,attr"`
-	Name    string   `xml:"name,attr"`
 	Valutes []Valute `xml:"Valute"`
 }
 
 type Valute struct {
-	ID       string  `xml:"ID,attr"`
 	NumCode  int     `xml:"NumCode"`
 	CharCode string  `xml:"CharCode"`
-	Nominal  int     `xml:"Nominal"`
-	Name     string  `xml:"Name"`
 	Value    float64 `xml:"Value"`
-}
-
-type currencyValue float64
-
-func (cv *currencyValue) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) error {
-	var valueString string
-
-	err := decoder.DecodeElement(&valueString, &start)
-	if err != nil {
-		return fmt.Errorf("decode element: %w", err)
-	}
-
-	valueString = strings.Replace(valueString, ",", ".", 1)
-
-	value, err := strconv.ParseFloat(valueString, 64)
-	if err != nil {
-		return fmt.Errorf("parse float %q: %w", valueString, err)
-	}
-
-	*cv = currencyValue(value)
-
-	return nil
-}
-
-func (v *Valute) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) error {
-	type alias Valute
-
-	var temp struct {
-		alias
-		Value currencyValue `xml:"Value"`
-	}
-
-	err := decoder.DecodeElement(&temp, &start)
-	if err != nil {
-		return fmt.Errorf("decode element: %w", err)
-	}
-
-	*v = Valute(temp.alias)
-	v.Value = float64(temp.Value)
-
-	return nil
 }
 
 func ParseXMLFile(inputFile string) (*ValCurs, error) {
