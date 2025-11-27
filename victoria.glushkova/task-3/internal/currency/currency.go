18b610f37f1560a14be4e7a83d8d78ceaabae5fc
Vika <vikagluskova2006@gmail.com>
2025-11-28 02:20:40 +0300

    [TASK-3] fix huerv

--- victoria.glushkova/task-3/internal/currency/currency.go
+++ victoria.glushkova/task-3/internal/currency/currency.go
@@ -1,48 +1,50 @@
 package currency
 
 import (
-        "encoding/xml"
-        "fmt"
-        "sort"
-        "strconv"
-        "strings"
+	"encoding/xml"
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 type Currency struct {
-        NumCode  int          `json:"num_code"  xml:"NumCode"`
-        CharCode string       `json:"char_code" xml:"CharCode"`
-        Value    CurrencyValue `json:"value"     xml:"Value"`
+	NumCode  int     `json:"num_code"  xml:"NumCode"`
+	CharCode string  `json:"char_code" xml:"CharCode"`
+	Value    float64 `json:"value"     xml:"Value"`
 }
 
-type CurrencyValue float64
+func (c *Currency) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) error {
+	type alias Currency
+	var temp struct {
+		alias
+		Value string `xml:"Value"`
+	}
 
-func (cv *CurrencyValue) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) error {
-        var valueString string
+	err := decoder.DecodeElement(&temp, &start)
+	if err != nil {
+		return fmt.Errorf("decode element: %w", err)
+	}
 
-        err := decoder.DecodeElement(&valueString, &start)
-        if err != nil {
-                return fmt.Errorf("decode element: %w", err)
-        }
+	*c = Currency(temp.alias)
 
-        valueString = strings.Replace(valueString, ",", ".", 1)
+	valueString := strings.Replace(temp.Value, ",", ".", 1)
+	value, err := strconv.ParseFloat(valueString, 64)
+	if err != nil {
+		return fmt.Errorf("parse float %q: %w", valueString, err)
+	}
 
-        value, err := strconv.ParseFloat(valueString, 64)
-        if err != nil {
-                return fmt.Errorf("parse float %q: %w", valueString, err)
-        }
-
-        *cv = CurrencyValue(value)
-
-        return nil
+	c.Value = value
+	return nil
 }
 
 func ConvertAndSort(currencies []Currency) []Currency {
-        result := make([]Currency, len(currencies))
-        copy(result, currencies)
+	result := make([]Currency, len(currencies))
+	copy(result, currencies)
 
-        sort.Slice(result, func(i, j int) bool {
-                return float64(result[i].Value) > float64(result[j].Value)
-        })
+	sort.Slice(result, func(i, j int) bool {
+		return result[i].Value > result[j].Value
+	})
 
-        return result
+	return result
 }
