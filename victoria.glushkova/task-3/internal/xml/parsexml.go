dc5089af94d3d7a52be1795e0e47b1612b45c073
Vika <vikagluskova2006@gmail.com>
2025-11-28 00:13:26 +0300

    [TASK-3] fix align struct tags

--- victoria.glushkova/task-3/internal/xml/parsexml.go
+++ victoria.glushkova/task-3/internal/xml/parsexml.go
@@ -17,9 +17,9 @@ type ValCurs struct {
 }
 
 type Valute struct {
-	NumCode  int          `json:"num_code" xml:"NumCode"`
+	NumCode  int          `json:"num_code"  xml:"NumCode"`
 	CharCode string       `json:"char_code" xml:"CharCode"`
-	Value    CurrencyValue `json:"value" xml:"Value"`
+	Value    CurrencyValue `json:"value"     xml:"Value"`
 }
 
 func ParseXMLFile(inputFile string) (*ValCurs, error) {
