0e5a0356a2096274ad68bc2fa0bc85fb21c16d30
Vika <vikagluskova2006@gmail.com>
2025-11-28 02:08:38 +0300

    [TASK-3] fix ujcgjlb

--- victoria.glushkova/task-3/internal/xml/parsexml.go
+++ victoria.glushkova/task-3/internal/xml/parsexml.go
@@ -1,39 +1,39 @@
 package xml
 
 import (
-	"bytes"
-	"encoding/xml"
-	"fmt"
-	"io"
-	"os"
+        "bytes"
+        "encoding/xml"
+        "fmt"
+        "io"
+        "os"
 
-	"golang.org/x/net/html/charset"
+        "golang.org/x/net/html/charset"
 
-	"github.com/vikaglushkova/task-3/internal/currency"
+        "github.com/vikaglushkova/task-3/internal/currency"
 )
 
 type ValCurs struct {
-	XMLName xml.Name           `xml:"ValCurs"`
-	Valutes []currency.Currency `xml:"Valute"`
+        XMLName xml.Name           `xml:"ValCurs"`
+        Valutes []currency.Currency `xml:"Valute"`
 }
 
 func ParseXMLFile(inputFile string) (*ValCurs, error) {
-	data, err := os.ReadFile(inputFile)
-	if err != nil {
-		return nil, fmt.Errorf("cannot read xml file: %w", err)
-	}
+        data, err := os.ReadFile(inputFile)
+        if err != nil {
+                return nil, fmt.Errorf("cannot read xml file: %w", err)
+        }
 
-	decoder := xml.NewDecoder(bytes.NewReader(data))
-	decoder.CharsetReader = func(encoding string, input io.Reader) (io.Reader, error) {
-		return charset.NewReader(input, encoding)
-	}
+        decoder := xml.NewDecoder(bytes.NewReader(data))
+        decoder.CharsetReader = func(encoding string, input io.Reader) (io.Reader, error) {
+                return charset.NewReader(input, encoding)
+        }
 
-	var valCurs ValCurs
+        var valCurs ValCurs
 
-	err = decoder.Decode(&valCurs)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse XML: %w", err)
-	}
+        err = decoder.Decode(&valCurs)
+        if err != nil {
+                return nil, fmt.Errorf("failed to parse XML: %w", err)
+        }
 
-	return &valCurs, nil
+        return &valCurs, nil
 }
