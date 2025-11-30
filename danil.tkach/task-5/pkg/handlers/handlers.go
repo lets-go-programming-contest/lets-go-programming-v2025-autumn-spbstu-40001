e0dfd4ac4fa142f9b5b997086967b6943fad4065
Danil3352 <danil.tkach2018@yandex.ru>
2025-11-30 22:59:36 +0300

    [TASK-5] Add handler: multiplexerFunc

--- danil.tkach/task-5/pkg/handlers/handlers.go
+++ danil.tkach/task-5/pkg/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"strings"
+	"sync"
 )
 
 var ErrNoDecorator = errors.New("can't be decorated")
@@ -63,4 +64,43 @@ func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string
 			}
 		}
 	}
+}
+
+func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
+	var wgr sync.WaitGroup
+
+	multiplex := func(chn chan string) {
+		defer wgr.Done()
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case data, ok := <-chn:
+				if !ok {
+					return
+				}
+
+				if strings.Contains(data, "no multiplexer") {
+					continue
+				}
+
+				select {
+				case output <- data:
+				case <-ctx.Done():
+					return
+				}
+			}
+		}
+	}
+
+	for _, chn := range inputs {
+		wgr.Add(1)
+		localCh := chn
+		go multiplex(localCh)
+	}
+
+	wgr.Wait()
+
+	return nil
 }
