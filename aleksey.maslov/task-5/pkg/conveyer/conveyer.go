770e339c4c035a0b76b0ba8abcd3a098567dfa3b
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-12-05 19:11:03 +0300

    [TASK-5] create base interface

---
+++ aleksey.maslov/task-5/pkg/conveyer/conveyer.go
@@ -0,0 +1,32 @@
+package conveyer
+
+import (
+	"context"
+	"errors"
+)
+
+var (
+	ErrChanNotFound = errors.New("chan not found")
+)
+
+type Conveyer interface {
+	RegisterDecorator(
+		fn func(ctx context.Context, input chan string, output chan string) error,
+		input string,
+		output string,
+	)
+	RegisterMultiplexer(
+		fn func(ctx context.Context, inputs []chan string, output chan string) error,
+		inputs []string,
+		output string,
+	)
+	RegisterSeparator(
+		fn func(ctx context.Context, input chan string, outputs []chan string) error,
+		input string,
+		outputs []string,
+	)
+
+	Run(ctx context.Context) error
+	Send(input string, data string) error
+	Recv(output string) (string, error)
+}
