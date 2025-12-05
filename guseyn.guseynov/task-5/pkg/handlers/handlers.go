f3233d810db660a68635418884c4ea184adc3eb2
GuseynovGuseynGG <ganx.gamer@gmail.com>
2025-12-05 21:27:55 +0300

    [TASK-5] maybe work

--- guseyn.guseynov/task-5/pkg/handlers/handlers.go
+++ guseyn.guseynov/task-5/pkg/handlers/handlers.go
@@ -4,13 +4,12 @@ import (
 	"context"
 	"errors"
 	"strings"
-	"sync"
 )
 
 var errPrefixDecoratorFuncCantBeDecorated = errors.New("handlers.PrefixDecoratorFunc: can't be decorated")
 
 func PrefixDecoratorFunc(
-	ctx context.Context,
+	context context.Context,
 	input chan string,
 	output chan string,
 ) error {
@@ -18,11 +17,11 @@ func PrefixDecoratorFunc(
 
 	for {
 		select {
-		case <-ctx.Done():
-			return ctx.Err()
+		case <-context.Done():
+			return nil
 
-		case data, isOpen := <-input:
-			if !isOpen {
+		case data, ok := <-input:
+			if !ok {
 				return nil
 			}
 
@@ -31,18 +30,15 @@ func PrefixDecoratorFunc(
 			}
 
 			if strings.HasPrefix(data, "decorated: ") {
-				select {
-				case <-ctx.Done():
-					return ctx.Err()
-				case output <- data:
-				}
+				output <- data
 
 				continue
 			}
 
 			select {
-			case <-ctx.Done():
-				return ctx.Err()
+			case <-context.Done():
+				return nil
+
 			case output <- "decorated: " + data:
 			}
 		}
@@ -50,27 +46,32 @@ func PrefixDecoratorFunc(
 }
 
 func MultiplexerFunc(
-	ctx context.Context,
+	context context.Context,
 	inputs []chan string,
 	output chan string,
 ) error {
 	defer close(output)
 
-	var wg sync.WaitGroup
-	errChan := make(chan error, 1)
+	activeInputs := make([]chan string, len(inputs))
+	copy(activeInputs, inputs)
 
-	for _, inputChan := range inputs {
-		wg.Add(1)
-		go func(in chan string) {
-			defer wg.Done()
+	for len(activeInputs) > 0 {
+		select {
+		case <-context.Done():
+			return nil
 
-			for {
+		default:
+			for i := 0; i < len(activeInputs); i++ {
 				select {
-				case <-ctx.Done():
-					return
-				case data, isOpen := <-in:
-					if !isOpen {
-						return
+				case <-context.Done():
+					return nil
+
+				case data, ok := <-activeInputs[i]:
+					if !ok {
+						activeInputs = append(activeInputs[:i], activeInputs[i+1:]...)
+						i--
+
+						continue
 					}
 
 					if strings.Contains(data, "no multiplexer") {
@@ -78,78 +79,47 @@ func MultiplexerFunc(
 					}
 
 					select {
-					case <-ctx.Done():
-						return
+					case <-context.Done():
+						return nil
+
 					case output <- data:
 					}
-				}
-			}
-		}(inputChan)
-	}
-
-	go func() {
-		wg.Wait()
-		errChan <- nil
-	}()
-
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case err := <-errChan:
-		return err
-	}
-}
-
-func SeparatorFunc(
-	ctx context.Context,
-	input chan string,
-	outputs []chan string,
-) error {
-	onceList := make([]*sync.Once, len(outputs))
-	for idx := range onceList {
-		onceList[idx] = &sync.Once{}
-	}
-
-	defer func() {
-		for idx, output := range outputs {
-			onceList[idx].Do(func() {
-				close(output)
-			})
-		}
-	}()
-
-	counter := 0
-
-	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-
-		case data, isOpen := <-input:
-			if !isOpen {
-				return nil
-			}
-
-			sent := false
-			for attempt := 0; attempt < len(outputs) && !sent; attempt++ {
-				idx := (counter + attempt) % len(outputs)
-				select {
-				case <-ctx.Done():
-					return ctx.Err()
-				case outputs[idx] <- data:
-					counter = idx + 1
-					sent = true
 				default:
 				}
 			}
+		}
+	}
 
-			if !sent {
-				select {
-				case <-ctx.Done():
-					return ctx.Err()
-				case outputs[counter%len(outputs)] <- data:
-					counter++
-				}
+	return nil
+}
+
+func SeparatorFunc(
+	context context.Context,
+	input chan string,
+	outputs []chan string,
+) error {
+	defer (func() {
+		for _, output := range outputs {
+			close(output)
+		}
+	})()
+
+	for i := 0; ; {
+		select {
+		case <-context.Done():
+			return nil
+
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			select {
+			case <-context.Done():
+				return nil
+
+			case outputs[i%len(outputs)] <- data:
+				i++
 			}
 		}
 	}
