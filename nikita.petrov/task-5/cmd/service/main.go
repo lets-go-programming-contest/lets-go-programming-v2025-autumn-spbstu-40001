4dbf5f06294261cee538c484f189e63099d03e0a
Nekich06 <p.nikita.73526@gmail.com>
2025-12-05 23:50:25 +0300

    [TASK-5] Implemented unification of modifiers types and added mutex to Conveyer struct

---
+++ nikita.petrov/task-5/cmd/service/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/Nekich06/task-5/pkg/conveyer"
+	"github.com/Nekich06/task-5/pkg/handlers"
+	"golang.org/x/sync/errgroup"
+)
+
+func ReadAll(name string, recv func(name string) (string, error)) ([]string, error) {
+	results := []string{}
+
+	for {
+		data, err := recv(name)
+		if err != nil {
+			return nil, err
+		}
+
+		results = append(results, data)
+		if data == undefinedData {
+			break
+		}
+	}
+
+	return results, nil
+}
+
+const (
+	chansSize          = 256
+	undefinedData      = "undefined"
+	chanNotExitMessage = "chan not found"
+)
+
+func main() {
+	stringsProcessor := conveyer.New(chansSize)
+	stringsProcessor.RegisterDecorator(handlers.PrefixDecoratorFunc, "input", "d1-output")
+	stringsProcessor.RegisterDecorator(handlers.PrefixDecoratorFunc, "d1-output", "output")
+
+	ctx, cancel := context.WithTimeout(context.TODO(), time.Second)
+	defer cancel()
+
+	errgr, ctx := errgroup.WithContext(ctx)
+	errgr.Go(func() error {
+		return stringsProcessor.Run(ctx)
+	})
+
+	err := stringsProcessor.Send("input", "val-1")
+	if err != nil {
+		panic(err)
+	}
+	err = stringsProcessor.Send("input", "decorated: val-2")
+	if err != nil {
+		panic(err)
+	}
+
+	errgr.Wait()
+
+	output, err := ReadAll("output", stringsProcessor.Recv)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(output)
+}
