71bc0662c9bc31978a1141d8fdc94b92f8a6b1c3
Vika <vikagluskova2006@gmail.com>
2025-12-05 21:45:30 +0300

    [TASK-5] Fix lint3

--- victoria.glushkova/task-5/pkg/handlers/handlers.go
+++ victoria.glushkova/task-5/pkg/handlers/handlers.go
@@ -7,9 +7,7 @@ import (
 	"sync"
 )
 
-var (
-	ErrNoDecorator = errors.New("can't be decorated")
-)
+var ErrNoDecorator = errors.New("can't be decorated")
 
 const (
 	noDecoratorData   = "no decorator"
