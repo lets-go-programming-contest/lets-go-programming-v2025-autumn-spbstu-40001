6f837172e99c05228c10990f60fb173ce9e44f0d
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-12-05 23:08:55 +0300

    [TASK-5] add separator

--- aleksey.maslov/task-5/pkg/handlers/handlers.go
+++ aleksey.maslov/task-5/pkg/handlers/handlers.go
@@ -49,6 +49,38 @@ func PrefixDecoratorFunc(
 	}
 }
 
+func SeparatorFunc(
+	ctx context.Context,
+	input chan string,
+	outputs []chan string,
+) error {
+	if len(outputs) == 0 {
+		return ErrEmptyChannel
+	}
+
+	var index int
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case message, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			targetChannel := outputs[index]
+			index = (index + 1) % len(outputs)
+
+			select {
+			case targetChannel <- message:
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	}
+}
+
 func MultiplexerFunc(
 	ctx context.Context,
 	inputs []chan string,
