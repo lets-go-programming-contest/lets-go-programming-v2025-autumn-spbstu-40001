caddd81e4ff32cc9e3cc525d0df667037835dd30
DimasFantomasA <genapupin2000@gmail.com>
2025-12-14 18:34:52 +0300

    [TASK-5] Fixing conveyer

--- trojan.dmitry/task-5/pkg/conveyer/conveyer.go
+++ trojan.dmitry/task-5/pkg/conveyer/conveyer.go
@@ -39,11 +39,6 @@ type Conveyer struct {
 	decorators   []decoratorHandler
 	multiplexers []multiplexerHandler
 	separators   []separatorHandler
-
-	runCtx    context.Context
-	runCancel context.CancelFunc
-	running   bool
-	runningMu sync.RWMutex
 }
 
 func New(size int) *Conveyer {
@@ -136,12 +131,10 @@ func (c *Conveyer) Send(input string, data string) error {
 		return err
 	}
 
-	select {
-	case ch <- data:
-		return nil
-	case <-c.runCtx.Done():
-		return c.runCtx.Err()
-	}
+	defer func() { _ = recover() }()
+
+	ch <- data
+	return nil
 }
 
 func (c *Conveyer) Recv(output string) (string, error) {
@@ -150,39 +143,15 @@ func (c *Conveyer) Recv(output string) (string, error) {
 		return "", err
 	}
 
-	select {
-	case v, ok := <-ch:
-		if !ok {
-			return UndefinedMsg, nil
-		}
-		return v, nil
-	case <-c.runCtx.Done():
-		return "", c.runCtx.Err()
+	v, ok := <-ch
+	if !ok {
+		return UndefinedMsg, nil
 	}
+	return v, nil
 }
 
 func (c *Conveyer) Run(ctx context.Context) error {
-	c.runningMu.Lock()
-	if c.running {
-		c.runningMu.Unlock()
-		return errors.New("conveyer is already running")
-	}
-
-	c.runCtx, c.runCancel = context.WithCancel(ctx)
-	c.running = true
-	c.runningMu.Unlock()
-
-	defer func() {
-		c.runningMu.Lock()
-		c.running = false
-		if c.runCancel != nil {
-			c.runCancel()
-			c.runCancel = nil
-		}
-		c.runningMu.Unlock()
-	}()
-
-	g, ctx := errgroup.WithContext(c.runCtx)
+	g, ctx := errgroup.WithContext(ctx)
 
 	for _, d := range c.decorators {
 		in := c.ensureChan(d.input)
@@ -233,12 +202,4 @@ func (c *Conveyer) Run(ctx context.Context) error {
 	c.chansMu.Unlock()
 
 	return err
-}
-
-func (c *Conveyer) Stop() {
-	c.runningMu.RLock()
-	if c.runCancel != nil {
-		c.runCancel()
-	}
-	c.runningMu.RUnlock()
 }
