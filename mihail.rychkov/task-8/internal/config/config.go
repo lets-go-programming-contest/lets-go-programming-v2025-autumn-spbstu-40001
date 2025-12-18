a1b73ba874878c8e80a1f5666113f1be36dbc9a1
Richmick <ry4ckov.misha2018@yandex.ru>
2025-12-18 16:19:23 +0300

    [TASK-8] Add dev-tag config switch

--- mihail.rychkov/task-8/internal/config/config.go
+++ mihail.rychkov/task-8/internal/config/config.go
@@ -1,11 +1,17 @@
 package config;
 
-import _ "gopkg.in/yaml.v3";
+import "fmt";
+import "gopkg.in/yaml.v3";
 
 type Config struct {
 	Env    string `yaml:"environment"`
 	LogLvl string `yaml:"log_level"`
 }
 func GetActive() (Config, error) {
-	return Config{"test", "test-lvl"}, nil;
+	var result Config;
+	err := yaml.Unmarshal(activeConfigRaw, &result)
+	if (err != nil) {
+		return result, fmt.Errorf("failed to parse active config: %w", err);
+	}
+	return result, nil;
 }
