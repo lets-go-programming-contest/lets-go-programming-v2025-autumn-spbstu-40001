f9bef2704310246de7dbf1322f64e35cb6e347b9
angelina.vladimirova <verticalochka@gmail.com>
2025-12-22 17:37:27 +0300

    [TASK-6] fix lint

--- angelina.vladimirova/task-6/internal/db/db.go
+++ angelina.vladimirova/task-6/internal/db/db.go
@@ -17,8 +17,33 @@ func New(db Database) DBService {
 	return DBService{DB: db}
 }
 
+// GetNames возвращает всех пользователей из таблицы
 func (service DBService) GetNames() ([]string, error) {
 	query := "SELECT name FROM users"
+	rows, err := service.DB.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("db query: %w", err)
+	}
+	defer rows.Close()
+
+	var names []string
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			return nil, fmt.Errorf("rows scanning: %w", err)
+		}
+		names = append(names, name)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("rows error: %w", err)
+	}
+
+	return names, nil
+}
+
+func (service DBService) GetUniqueNames() ([]string, error) {
+	query := "SELECT DISTINCT name FROM users"
 	rows, err := service.DB.Query(query)
 	if err != nil {
 		return nil, fmt.Errorf("db query: %w", err)
