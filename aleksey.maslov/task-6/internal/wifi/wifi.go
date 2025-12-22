b6cda9e1efd606b731245de92a4bf0d2dc294b85
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-12-22 22:59:46 +0300

    [TASK-6] add wifi service

--- aleksey.maslov/task-6/internal/wifi/wifi.go
+++ aleksey.maslov/task-6/internal/wifi/wifi.go
@@ -1,1 +1,47 @@
 package wifi
+
+import (
+	"net"
+
+	mdwifi "github.com/mdlayher/wifi"
+)
+
+type WiFiHandle interface {
+	Interfaces() ([]*mdwifi.Interface, error)
+}
+
+type Service struct {
+	handle WiFiHandle
+}
+
+func New(handle WiFiHandle) *Service {
+	return &Service{handle: handle}
+}
+
+func (s *Service) GetAddresses() ([]net.HardwareAddr, error) {
+	ifaces, err := s.handle.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	addrs := make([]net.HardwareAddr, 0, len(ifaces))
+	for _, iface := range ifaces {
+		addrs = append(addrs, iface.HardwareAddr)
+	}
+
+	return addrs, nil
+}
+
+func (s *Service) GetNames() ([]string, error) {
+	ifaces, err := s.handle.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	names := make([]string, 0, len(ifaces))
+	for _, iface := range ifaces {
+		names = append(names, iface.Name)
+	}
+
+	return names, nil
+}
