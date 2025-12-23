bbc675a39e13f557a4e809aa597a1ff71745d5e3
lee <likachich12@gmail.com>
2025-12-23 17:53:25 +0300

    [TASK-6] fixing db

--- elizaveta.hismatova/task-6/internal/db/db.go
+++ elizaveta.hismatova/task-6/internal/db/db.go
@@ -13,13 +13,11 @@ type DBService struct {
 	DB Database
 }
 
-func New(db Database) DBService {
-	return DBService{DB: db}
+func New(db Database) *DBService {
+	return &DBService{DB: db}
 }
 
-func (service DBService) GetNames() ([]string, error) {
-	query := "SELECT name FROM users"
-
+func (service *DBService) queryNames(query string) ([]string, error) {
 	rows, err := service.DB.Query(query)
 	if err != nil {
 		return nil, fmt.Errorf("db query: %w", err)
@@ -45,30 +43,10 @@ func (service DBService) GetNames() ([]string, error) {
 	return names, nil
 }
 
-func (service DBService) GetUniqueNames() ([]string, error) {
-	query := "SELECT DISTINCT name FROM users"
+func (service *DBService) GetNames() ([]string, error) {
+	return service.queryNames("SELECT name FROM users")
+}
 
-	rows, err := service.DB.Query(query)
-	if err != nil {
-		return nil, fmt.Errorf("db query: %w", err)
-	}
-	defer rows.Close()
-
-	var values []string
-
-	for rows.Next() {
-		var value string
-
-		if err := rows.Scan(&value); err != nil {
-			return nil, fmt.Errorf("rows scanning: %w", err)
-		}
-
-		values = append(values, value)
-	}
-
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("rows error: %w", err)
-	}
-
-	return values, nil
+func (service *DBService) GetUniqueNames() ([]string, error) {
+        return service.queryNames("SELECT DISTINCT name FROM users")
 }
