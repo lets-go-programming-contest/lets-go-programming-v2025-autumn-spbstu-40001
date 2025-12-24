c05961aa555e4e0118ab9e54ce631b2cb9e2317a
Alexander Alymov <aleksander.alymov@gmail.com>
2025-12-24 16:27:44 +0300

    [TASK-3] Fix XML parsing and currency code handling

--- aleksander.alymov/task-3/internal/json/json.go
+++ aleksander.alymov/task-3/internal/json/json.go
@@ -7,35 +7,19 @@ import (
 	"path/filepath"
 )
 
-type FileSaver interface {
-	Save(filename string, data interface{}) error
-}
-
-type JSONSaver struct {
-	DirPerms  os.FileMode
-	FilePerms os.FileMode
-}
-
-func NewSaver() *JSONSaver {
-	return &JSONSaver{
-		DirPerms:  0755,
-		FilePerms: 0644,
-	}
-}
-
-func (s *JSONSaver) Save(filename string, data interface{}) error {
-	outputDir := filepath.Dir(filename)
-	if err := os.MkdirAll(outputDir, s.DirPerms); err != nil {
-		return fmt.Errorf("create directory: %w", err)
+func WriteResult[T any](data T, outputFile string) error {
+	outputDir := filepath.Dir(outputFile)
+	if err := os.MkdirAll(outputDir, 0755); err != nil {
+		return fmt.Errorf("failed to create a dir %s: %w", outputDir, err)
 	}
 
 	jsonData, err := json.MarshalIndent(data, "", "  ")
 	if err != nil {
-		return fmt.Errorf("marshal JSON: %w", err)
+		return fmt.Errorf("failed to marshal data to json: %w", err)
 	}
 
-	if err := os.WriteFile(filename, jsonData, s.FilePerms); err != nil {
-		return fmt.Errorf("write file: %w", err)
+	if err := os.WriteFile(outputFile, jsonData, 0644); err != nil {
+		return fmt.Errorf("failed write file %s: %w", outputFile, err)
 	}
 
 	return nil
