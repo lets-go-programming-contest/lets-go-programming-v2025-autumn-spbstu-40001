1d9e3162274bb918fd54d12ffe66a1596563053c
Aapng-cmd <verart1@yandex.ru>
2025-12-27 05:05:08 -0600

    [TASK-2-1] Full task for automatic

--- artemiy.vernikovskiy/task-2-1/cmd/service/main.go
+++ artemiy.vernikovskiy/task-2-1/cmd/service/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"os"
 
+	"github.com/Aapng-cmd/task-2-1/internal/commander"
+	"github.com/Aapng-cmd/task-2-1/internal/models"
 )
 
 func main() {
+	wrapper := models.Wrapper{Cin: os.Stdin, Cout: os.Stdout}
 
+	err := commander.CalcForDepartment(&wrapper)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
-
--- artemiy.vernikovskiy/task-2-1/go.mod
+++ artemiy.vernikovskiy/task-2-1/go.mod
@@ -1,3 +1,3 @@
 module github.com/Aapng-cmd/task-2-1
 
-go 1.24.9
+go 1.22.7
