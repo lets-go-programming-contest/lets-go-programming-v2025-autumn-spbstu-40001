00fe7a20220503abf81a833e6a6e85a5a1e1a132
Aapng-cmd <verart1@yandex.ru>
2025-12-27 05:13:16 -0600

    [TASK-2-2] Full task for automatic

--- artemiy.vernikovskiy/task-2-2/cmd/service/main.go
+++ artemiy.vernikovskiy/task-2-2/cmd/service/main.go
@@ -1,10 +1,54 @@
 package main
 
 import (
+	"container/heap"
+	"fmt"
 
+	"github.com/Aapng-cmd/task-2-2/internal/myheap"
 )
 
 func main() {
+	var (
+		nDishesNumber, actualDish, wanting int
+		workHeap                           = &myheap.Heap{}
+	)
 
+	_, err := fmt.Scan(&nDishesNumber)
+	if err != nil {
+		fmt.Println("Invalid dishes count")
+
+		return
+	}
+
+	heap.Init(workHeap)
+
+	for range nDishesNumber {
+		_, err = fmt.Scan(&actualDish)
+		if err != nil {
+			fmt.Println("invalid input for dish number: ", err)
+
+			return
+		}
+
+		heap.Push(workHeap, actualDish)
+	}
+
+	_, err = fmt.Scan(&wanting)
+	if err != nil {
+		fmt.Println("invalid input for wanted dish: ", err)
+
+		return
+	}
+
+	for range wanting - 1 {
+		if workHeap.Len() == 0 {
+			fmt.Println("no dish for you")
+
+			return
+		} // здесь надо проверять, потому что Pop может удалить слишком много
+
+		heap.Pop(workHeap)
+	}
+
+	fmt.Println(heap.Pop(workHeap))
 }
-
--- artemiy.vernikovskiy/task-2-2/go.mod
+++ artemiy.vernikovskiy/task-2-2/go.mod
@@ -1,3 +1,3 @@
 module github.com/Aapng-cmd/task-2-2
 
-go 1.24.9
+go 1.22.7
