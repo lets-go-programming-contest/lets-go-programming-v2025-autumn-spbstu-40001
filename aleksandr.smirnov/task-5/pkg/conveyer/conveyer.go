937f034fec3cda40de894474b668c48262842ff0
A1exCRE <alsmmm2065@gmail.com>
2026-01-05 18:14:58 +0300

    [TASK-5] implement channel management functions

--- aleksandr.smirnov/task-5/pkg/conveyer/conveyer.go
+++ aleksandr.smirnov/task-5/pkg/conveyer/conveyer.go
@@ -11,12 +11,12 @@ var ErrChannelNotFound = errors.New("chan not found")
 const undefined = "undefined"
 
 type Pipeline struct {
-	size      int
-	channels  map[string]chan string
-	mu        sync.RWMutex
-	modifiers []modifierEntry
-	muxers    []muxEntry
-	splitters []splitterEntry
+	size       int
+	channels   map[string]chan string
+	muChannels sync.RWMutex
+	modifiers  []modifierEntry
+	muxers     []muxEntry
+	splitters  []splitterEntry
 }
 
 type modifierEntry struct {
@@ -39,11 +39,43 @@ type splitterEntry struct {
 
 func New(size int) *Pipeline {
 	return &Pipeline{
-		size:      size,
-		channels:  make(map[string]chan string),
-		mu:        sync.RWMutex{},
-		modifiers: make([]modifierEntry, 0),
-		muxers:    make([]muxEntry, 0),
-		splitters: make([]splitterEntry, 0),
+		size:       size,
+		channels:   make(map[string]chan string),
+		muChannels: sync.RWMutex{},
+		modifiers:  make([]modifierEntry, 0),
+		muxers:     make([]muxEntry, 0),
+		splitters:  make([]splitterEntry, 0),
+	}
+}
+
+func (p *Pipeline) getOrCreateChannel(name string) chan string {
+	p.muChannels.Lock()
+	defer p.muChannels.Unlock()
+
+	if ch, exists := p.channels[name]; exists {
+		return ch
+	}
+
+	ch := make(chan string, p.size)
+	p.channels[name] = ch
+
+	return ch
+}
+
+func (p *Pipeline) getChannel(name string) (chan string, bool) {
+	p.muChannels.RLock()
+	defer p.muChannels.RUnlock()
+
+	ch, exists := p.channels[name]
+
+	return ch, exists
+}
+
+func (p *Pipeline) closeAllChannels() {
+	p.muChannels.Lock()
+	defer p.muChannels.Unlock()
+
+	for _, ch := range p.channels {
+		close(ch)
 	}
 }
