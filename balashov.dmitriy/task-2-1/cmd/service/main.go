96fa34a0bcbaaf5897162cbbc9ac62a1b885efed
DmitryBalashov <olegworker665@gmail.com>
2026-01-08 21:52:03 +0300

    [TASK-2-1] Add loop for departments and employee preferences reading

--- balashov.dmitriy/task-2-1/cmd/service/main.go
+++ balashov.dmitriy/task-2-1/cmd/service/main.go
@@ -5,11 +5,32 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
+)
+
+const (
+	minTemp = 15
+	maxTemp = 30
 )
 
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
 	n, _ := strconv.Atoi(sc.Text())
-	fmt.Println(n)
+
+	for i := 0; i < n; i++ {
+		sc.Scan()
+		k, _ := strconv.Atoi(sc.Text())
+
+		low := minTemp
+		high := maxTemp
+
+		for j := 0; j < k; j++ {
+			sc.Scan()
+			parts := strings.Split(sc.Text(), " ")
+			t, _ := strconv.Atoi(parts[1])
+			op := parts[0]
+			fmt.Printf("%s %d\n", op, t)
+		}
+	}
 }
