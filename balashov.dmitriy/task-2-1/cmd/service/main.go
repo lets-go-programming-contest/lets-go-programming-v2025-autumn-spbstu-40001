23641a64233bbd226cb102693e3a412ead6c7ab9
DmitryBalashov <olegworker665@gmail.com>
2026-01-08 21:48:58 +0300

    [TASK-2-1] Initial setup

---
+++ balashov.dmitriy/task-2-1/cmd/service/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	n, _ := strconv.Atoi(sc.Text())
+	fmt.Println(n)
+}
