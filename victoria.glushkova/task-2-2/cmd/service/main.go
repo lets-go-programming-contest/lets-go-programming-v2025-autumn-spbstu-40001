c773035ae218200389d9892dfbe2da25e0e9a771
vikaglushkova <vikagluskova2006@gmail.com>
2026-01-09 05:21:42 +0300

    [TASK-2-2] (#150) 1/3
    * [TASK-2-2] Initial commit
    
    * [TASK-2-2] add go.mod file
    
    * [TASK-2-2] Fix lint
    
    * [TASK-2-2] Fix remaining linter issues
    
    * [TASK-2-2] Fix all linter errors
    
    * [TASK-2-2] Implement dish rating selection using heap
    
    * [TASK-2-2] solution with heap implementation
    
    * [TASK-2-2] Fix cyclomatic complexity and formatting
    
    * [TASK-2-2] Fix all formatting issues
    
    * [TASK-2-2] Fix linter2
    
    * [TASK-2-2] Fix linter2

---
+++ victoria.glushkova/task-2-2/cmd/service/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type IntHeap []int
+
+func (h *IntHeap) Len() int { return len(*h) }
+
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
+
+func (h *IntHeap) Swap(i, j int) { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	rating, ok := x.(int)
+	if !ok {
+		return
+	}
+
+	*h = append(*h, rating)
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	length := len(old)
+	item := old[length-1]
+	*h = old[0 : length-1]
+
+	return item
+}
+
+func main() {
+	var totalDishes int
+
+	_, err := fmt.Scan(&totalDishes)
+	if err != nil {
+		fmt.Println("Error:", err)
+
+		return
+	}
+
+	heapInstance := &IntHeap{}
+	heap.Init(heapInstance)
+
+	for range totalDishes {
+		var rating int
+
+		_, err := fmt.Scan(&rating)
+		if err != nil {
+			fmt.Println("Error:", err)
+
+			return
+		}
+
+		heap.Push(heapInstance, rating)
+	}
+
+	var preferenceOrder int
+
+	_, err = fmt.Scan(&preferenceOrder)
+	if err != nil {
+		fmt.Println("Error:", err)
+
+		return
+	}
+
+	tempHeap := make(IntHeap, len(*heapInstance))
+	copy(tempHeap, *heapInstance)
+	heap.Init(&tempHeap)
+
+	var result int
+
+	for range preferenceOrder {
+		item := heap.Pop(&tempHeap)
+		rating, ok := item.(int)
+
+		if !ok {
+			fmt.Println("Error: unexpected type")
+
+			return
+		}
+
+		result = rating
+	}
+
+	fmt.Println(result)
+}
